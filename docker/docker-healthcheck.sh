e3ef5cd830e3ef7fa4304d7cbf66df1916a4252c
frasergr <frasergr@users.noreply.github.com>
2023-06-12 00:26:37 -0700

    updates for collector in docker

---
+++ docker/docker-healthcheck.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Send a request to the specified URL
+response=$(curl --write-out '%{http_code}' --silent --output /dev/null http://localhost:3001/api/ping)
+
+# If the HTTP response code is 200 (OK), the server is up
+if [ $response -eq 200 ]; then
+    echo "Server is up"
+    exit 0
+else
+    echo "Server is down"
+    exit 1
+fi
