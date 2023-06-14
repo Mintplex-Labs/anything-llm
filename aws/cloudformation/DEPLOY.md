4dd1887bd12778118e53cb87626d976a8ba83288
Timothy Carambat <rambat1010@gmail.com>
2023-06-14 10:34:22 -0700

    Easy AWS cloud deployment for private instances via CloudFormation template (#52)
    * wip
    
    * fix file ref
    
    * update dockerfile
    
    * mute chown
    
    * add build script for AWS CF template construction
    add comment about script and AWS deployment
    
    * move aws stuff into its own folder
    
    * edit readme

---
+++ aws/cloudformation/DEPLOY.md
@@ -0,0 +1,54 @@
+# How to deploy a private AnythingLLM instance on AWS
+
+With an AWS account you can easily deploy a private AnythingLLM instance on AWS. This will create a url that you can access from any browser over HTTP (HTTPS not supported). This single instance will run on your own keys and they will not be exposed - however if you want your instance to be protected it is highly recommend that you set the `AUTH_TOKEN` and `JWT_SECRET` variables in the `docker/` ENV.
+
+[Refer to .env.example](../../docker/HOW_TO_USE_DOCKER.md) for data format.
+
+The output of this cloudformation stack will be:
+- 1 EC2 Instance
+- 1 Security Group with 0.0.0.0/0 access on Ports 22 & 3001
+- 1 EC2 Instance Volume `gb2` of 10Gib minimum
+
+**Requirements**
+- An AWS account with billing information.
+  - AnythingLLM can run within the free tier using a t2.micro and 10Gib SSD hard disk volume
+- `.env` file that is filled out with your settings and set up in the `docker/` folder
+
+## How to deploy on AWS
+
+1. Generate your specific cloudformation document by running `yarn generate:cloudformation` from the project root directory.
+2. Log in to your AWS account
+3. Open [CloudFormation](https://us-west-1.console.aws.amazon.com/cloudformation/home)
+4. Ensure you are deploying in a geographic zone that is nearest to your physical location to reduce latency.
+5. Click `Create Stack`
+
+![Create Stack](/images/screenshots/create_stack.png)
+
+6. Upload your `aws_cf_deploy_anything_llm.json` to the stack
+
+![Upload Stack](/images/screenshots/upload.png)
+
+7. Click `Next` and give your stack a name. This is superficial.
+8. No other changes are needed, just proceed though each step
+9. Click `Submit`
+10. Wait for stack events to finish and be marked as `Completed`
+11. View `Outputs` tab.
+
+![Stack Output](/images/screenshots/cf_outputs.png)
+
+## Please read this notice before submitting issues about your deployment
+
+**Note:** 
+Your instance will not be available instantly. Depending on the instance size you launched with it can take anywhere from 10-20 minutes to fully boot up.
+
+If you want to check the instances progress, navigate to [your deployed EC2 instances](https://us-west-1.console.aws.amazon.com/ec2/home) and connect to your instance via SSH in browser.
+
+Once connected run `sudo tail -f /var/log/cloud-init-output.log` and wait for the file to conclude deployment of the docker image.
+You should see an output like this
+```
+[+] Running 2/2
+ ⠿ Network docker_anything-llm  Created 
+ ⠿ Container anything-llm       Started  
+```
+
+Additionally, your use of this deployment process means you are responsible for any costs of these AWS resources fully.
---
+++ aws/cloudformation/generate.mjs
@@ -0,0 +1,56 @@
+// Note (tcarambat) This script should be executed from root via the `yarn generate::cloudformation` command only.
+// This script will copy your current Docker .env settings being used into a slightly custom AWS CloudFormation template
+// that you can upload and deploy on AWS in a single click!
+// Recommended settings are already defined in the template but you can modify them as needed.
+// AnythingLLM can run within the free tier services of AWS (t2.micro w/10GB of storage)
+//
+// This will deploy a fully public AnythingLLM so if you do not want anyone to access it please set the AUTH_TOKEN & JWT_SECRET envs
+// before running this script. You can still run the collector scripts on AWS so no FTP or file uploads are required.
+// Your documents and data do not leave your AWS instance when you host in the cloud this way.
+
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { exit } from 'process';
+import chalk from 'chalk';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const REPLACEMENT_KEY = '!SUB::USER::CONTENT!'
+
+const envPath = path.resolve(__dirname, `../../docker/.env`)
+const envFileExists = fs.existsSync(envPath);
+
+if (!envFileExists) {
+  console.log(chalk.redBright('[ABORT]'), 'You do not have an .env file in your ./docker/ folder. You need to create it first.');
+  console.log('You can start by running', chalk.cyan('cp -n ./docker/.env.example ./docker/.env'))
+  exit(1);
+}
+
+// Remove comments
+// Remove UID,GID,etc
+// Remove empty strings
+// Split into array
+const settings = fs.readFileSync(envPath, "utf8")
+  .replace(/^#.*\n?/gm, '')
+  .replace(/^UID.*\n?/gm, '')
+  .replace(/^GID.*\n?/gm, '')
+  .replace(/^CLOUD_BUILD.*\n?/gm, '')
+  .replace(/^\s*\n/gm, "")
+  .split('\n')
+  .filter((i) => !!i)
+
+
+const templatePath = path.resolve(__dirname, `cf_template.template`);
+const templateString = fs.readFileSync(templatePath, "utf8");
+const template = JSON.parse(templateString);
+
+const cmdIdx = template.Resources.AnythingLLMInstance.Properties.UserData['Fn::Base64']['Fn::Join'][1].findIndex((cmd) => cmd === REPLACEMENT_KEY)
+template.Resources.AnythingLLMInstance.Properties.UserData['Fn::Base64']['Fn::Join'][1].splice(cmdIdx, 1, ...settings);
+
+const output = path.resolve(__dirname, `aws_cf_deploy_anything_llm.json`);
+fs.writeFileSync(output, JSON.stringify(template, null, 2), "utf8");
+
+console.log(chalk.greenBright('[SUCCESS]'), 'Deploy AnythingLLM on AWS CloudFormation using your template document.');
+console.log(chalk.greenBright('File Created:'), 'aws_cf_deploy_anything_llm.json in aws/cloudformation directory.');
+console.log(chalk.blueBright('[INFO]'), 'Refer to aws/cloudformation/DEPLOY.md for how to use this file.');
+
+exit();
