9b8533ad5fccd9db96d8e325f4e232497f7c2414
frasergr <frasergr@users.noreply.github.com>
2023-06-17 13:49:20 -0700

    dockerfile cleanup; enforce text LF line endings

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,3 +1,5 @@
-#!/usr/bin/env bash
-
-exec "$@"
+#!/bin/bash
+node /app/server/index.js &
+{ FLASK_ENV=production FLASK_APP=wsgi.py cd collector && gunicorn --workers 4 --bind 0.0.0.0:8888 wsgi:api; } &
+wait -n
+exit $?
