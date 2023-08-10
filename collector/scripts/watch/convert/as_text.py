b7d268cbd2908372ffe529044e7ae1e8bfa4f64d
Francisco Bischoff <franzbischoff@gmail.com>
2023-08-10 03:37:01 +0100

    small changes on other file ingestions in order to try to keep the fields equal

--- collector/scripts/watch/convert/as_text.py
+++ collector/scripts/watch/convert/as_text.py
@@ -14,7 +14,7 @@ def as_text(**kwargs):
 
   print(f"-- Working {fullpath} --")
   data = {
-    'id': guid(), 
+    'id': guid(),
     'url': "file://"+os.path.abspath(f"{parent_dir}/processed/{filename}{ext}"),
     'title': f"{filename}{ext}",
     'description': "a custom file uploaded by the user.",
@@ -23,7 +23,20 @@ def as_text(**kwargs):
     'pageContent': content,
     'token_count_estimate': len(tokenize(content))
   }
-  
+  data = {
+    'id': guid(),
+    'url': "file://"+os.path.abspath(f"{parent_dir}/processed/{slugify(filename)}-{guid()}{ext}"),
+    'title': slugify(filename), # TODO: Find a better title
+    'author': 'Unknown', # TODO: Find a better author
+    'description': 'Unknown', # TODO: Find a better description
+    'document_source': "plain text file uploaded by the user.",
+    'published': file_creation_time(fullpath),
+    'wordCount': len(content),
+    'pageContent': content,
+    'token_count_estimate': len(tokenize(content))
+  }
+
+
   write_to_server_documents(data, f"{slugify(filename)}-{data.get('id')}")
   move_source(parent_dir, f"{filename}{ext}", remove=remove)
   print(f"[SUCCESS]: {filename}{ext} converted & ready for embedding.\n")
