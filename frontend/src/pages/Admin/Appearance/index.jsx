4ab4dc724e5b89cf202905657802866c8daa3760
shatfield4 <seanhatfield5@gmail.com>
2023-08-15 12:11:03 -0700

    fixing img to use light/dark modes

--- frontend/src/pages/Admin/Appearance/index.jsx
+++ frontend/src/pages/Admin/Appearance/index.jsx
@@ -197,9 +197,12 @@ export default function Appearance() {
                 src={logo}
                 alt="Uploaded Logo"
                 className="w-48 h-48 object-contain mr-6"
-                onError={(e) => (e.target.src = defaultLogo)}
+                onError={(e) =>
+                  (e.target.src = prefersDarkMode
+                    ? AnythingLLMLight
+                    : AnythingLLMDark)
+                }
               />
-
               <div className="flex flex-col">
                 <div className="mb-4">
                   <label className="cursor-pointer text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
