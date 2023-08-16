2036204f23e5b5fe20860135fbad00684372a10d
timothycarambat <rambat1010@gmail.com>
2023-08-16 12:26:52 -0700

    default frontend api to relative URL

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const API_BASE =
-  import.meta.env.VITE_API_BASE || "http://localhost:3001/api";
+  import.meta.env.VITE_API_BASE || "/api";
 
 export const AUTH_USER = "anythingllm_user";
 export const AUTH_TOKEN = "anythingllm_authToken";
