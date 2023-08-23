c0adcc129d21d711cd922e2efd6c4e1530eb903d
Sean Hatfield <seanhatfield5@gmail.com>
2023-08-22 19:18:47 -0700

    Success fail messages for upload document (#208)
    * WIP success fail messages for upload document
    
    * added success/error msgs for uploading feedback and disabled fileUploadProgress in backend
    
    * remove unused middleware
    
    ---------
    
    Co-authored-by: timothycarambat <rambat1010@gmail.com>

--- frontend/src/components/Modals/MangeWorkspace/Upload/index.jsx
+++ frontend/src/components/Modals/MangeWorkspace/Upload/index.jsx
@@ -10,6 +10,19 @@ import { Frown } from "react-feather";
 export default function UploadToWorkspace({ workspace, fileTypes }) {
   const [ready, setReady] = useState(null);
   const [files, setFiles] = useState([]);
+  const [successMsg, setSuccessMsg] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const handleUploadSuccess = () => {
+    setSuccessMsg("File uploaded successfully");
+    setErrorMsg(null);
+  };
+
+  const handleUploadError = (message) => {
+    setErrorMsg(`Upload failed: ${message}`);
+    setSuccessMsg(null);
+  };
+
   const onDrop = useCallback(async (acceptedFiles, rejections) => {
     const newAccepted = acceptedFiles.map((file) => {
       return {
@@ -36,6 +49,20 @@ export default function UploadToWorkspace({ workspace, fileTypes }) {
     }
     checkProcessorOnline();
   }, []);
+
+  useEffect(() => {
+    if (!!successMsg) {
+      setTimeout(() => {
+        setSuccessMsg("");
+      }, 3_500);
+    }
+
+    if (!!errorMsg) {
+      setTimeout(() => {
+        setErrorMsg("");
+      }, 3_500);
+    }
+  }, [successMsg, errorMsg]);
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
@@ -133,6 +160,8 @@ export default function UploadToWorkspace({ workspace, fileTypes }) {
                 slug={workspace.slug}
                 rejected={file?.rejected}
                 reason={file?.reason}
+                onUploadSuccess={handleUploadSuccess}
+                onUploadError={handleUploadError}
               />
             ))}
           </div>
@@ -144,6 +173,16 @@ export default function UploadToWorkspace({ workspace, fileTypes }) {
           {Object.values(fileTypes).flat().join(" ")}
         </code>
       </p>
+      {successMsg && (
+        <p className="text-green-600 dark:text-green-400 text-sm text-center pt-2">
+          {successMsg}
+        </p>
+      )}
+      {errorMsg && (
+        <p className="text-red-600 dark:text-red-400 text-sm text-center pt-2">
+          {errorMsg}
+        </p>
+      )}
     </ModalWrapper>
   );
 }
