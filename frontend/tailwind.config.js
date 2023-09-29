6987bc4936372a7957ef69b759e65de76a55439c
Sean Hatfield <seanhatfield5@gmail.com>
2023-09-29 12:07:13 -0700

    v2 Login screen (#254)
    * adding gradients for modal and sidebar
    
    * adding font setup
    
    * redesigned login screen for MultiUserAuth
    
    * completed multi user mode login screen
    
    * linting
    
    * login screen for single user auth redesign complete
    
    * created reusable gradient for login screen
    
    ---------
    
    Co-authored-by: timothycarambat <rambat1010@gmail.com>

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,8 +6,18 @@ export default {
       colors: {
         'black-900': '#141414',
       },
+      backgroundImage: {
+        'modal-gradient': 'linear-gradient(180deg, #3D4147 0%, #2C2F35 100%)',
+        'sidebar-gradient': 'linear-gradient(90deg, #5B616A 0%, #3F434B 100%)',
+      },
+      fontFamily: {
+        'sans': ['plus-jakarta-sans', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
+      },
+      animation: {
+        'slow-pulse': 'subtlePulse 20s infinite',
+        'slow-shift': 'subtleShift 20s infinite',
+      }
     },
   },
   plugins: [],
 }
-
