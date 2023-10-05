a4c2c7d1759938712f3e24faf56e0707b2bab680
timothycarambat <rambat1010@gmail.com>
2023-10-05 15:20:12 -0700

    hide other 3 dot button

--- frontend/src/components/SettingsSidebar/index.jsx
+++ frontend/src/components/SettingsSidebar/index.jsx
@@ -162,7 +162,7 @@ export default function SettingsSidebar() {
                       className="h-5 w-5 stroke-slate-200 group-hover:stroke-slate-200"
                     />
                   </a>
-                  <button className="transition-all duration-300 p-2 rounded-full text-white bg-sidebar-button hover:bg-menu-item-selected-gradient hover:border-slate-100 hover:border-opacity-50 border-transparent border">
+                  <button className="invisible transition-all duration-300 p-2 rounded-full text-white bg-sidebar-button hover:bg-menu-item-selected-gradient hover:border-slate-100 hover:border-opacity-50 border-transparent border">
                     <DotsThree className="h-5 w-5 group-hover:stroke-slate-200" />
                   </button>
                 </div>
