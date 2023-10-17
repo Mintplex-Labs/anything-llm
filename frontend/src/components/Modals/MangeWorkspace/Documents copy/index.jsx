54f4fddca864f57bbbc21899210a6257e4f71a51
shatfield4 <seanhatfield5@gmail.com>
2023-10-17 15:44:52 -0700

    WIP refactor document picker to work with backend

---
+++ frontend/src/components/Modals/MangeWorkspace/Documents copy/index.jsx
@@ -0,0 +1,88 @@
+import { ArrowsDownUp } from "@phosphor-icons/react";
+import { useEffect, useState } from "react";
+import WorkspacePicker from "./WorkspacePicker";
+import MyDocumentsPicker from "./MyDocumentsPicker";
+import Workspace from "../../../../models/workspace";
+import System from "../../../../models/system";
+
+export default function DocumentSettings({ workspace }) {
+  const [selectedDocs, setSelectedDocs] = useState([]);
+  const [isWorkspaceHighlighted, setIsWorkspaceHighlighted] = useState(false);
+  const [availableDocs, setAvailableDocs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [workspaceDocs, setWorkspaceDocs] = useState([]);
+
+  // State for the active workspace, might be used for further workspace-specific operations
+  const [activeWorkspace, setActiveWorkspace] = useState(null);
+
+  const [fileDirectories, setFileDirectories] = useState([]);
+
+  // State indicating if the user has any files in their local system or specific directory
+  const [userHasFiles, setUserHasFiles] = useState(false);
+
+  // Fetching documents and determining their initial state
+  async function initializeDocuments(refetchWorkspace = false) {
+    setLoading(true);
+    const localFiles = await System.localFiles();
+    const currentWorkspace = refetchWorkspace
+      ? await Workspace.bySlug(workspace.slug)
+      : workspace;
+
+    console.log("FILE DIRECTORIES: ", localFiles);
+    console.log(currentWorkspace);
+
+    const documentsInWorkspace =
+      currentWorkspace.documents.map((doc) => doc.docpath) || [];
+    const isAnyLocalFiles = localFiles.items.some(
+      (folder) => folder?.items?.length > 0
+    );
+
+    setFileDirectories(localFiles);
+    setWorkspaceDocs(documentsInWorkspace);
+    setUserHasFiles(isAnyLocalFiles);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    initializeDocuments();
+  }, []);
+
+  const toggleDocSelection = (docId) => {
+    setSelectedDocs((prevSelectedDocs) =>
+      prevSelectedDocs.includes(docId)
+        ? prevSelectedDocs.filter((id) => id !== docId)
+        : [...prevSelectedDocs, docId]
+    );
+  };
+
+  const handleMoveToWorkspace = () => {
+    setAvailableDocs((prevDocs) =>
+      prevDocs.map((doc) =>
+        selectedDocs.includes(doc.id) ? { ...doc, location: 1 } : doc
+      )
+    );
+    setSelectedDocs([]);
+    setIsWorkspaceHighlighted(false);
+  };
+
+  useEffect(() => {
+    console.log(selectedDocs);
+  }, [selectedDocs]);
+
+  return (
+    <div className="flex gap-x-6 justify-center">
+      <MyDocumentsPicker fileDirectories={fileDirectories} loading={loading} />
+      <div className="flex items-center">
+        <ArrowsDownUp className="text-white text-base font-bold rotate-90 w-11 h-11" />
+      </div>
+      {/* <WorkspacePicker
+        workspace={workspace}
+        workspaceDocs={documentsInWorkspace}
+        selectedDocs={selectedDocs}
+        toggleDocSelection={toggleDocSelection}
+        isWorkspaceHighlighted={isWorkspaceHighlighted}
+        isLoading={isLoading}
+      /> */}
+    </div>
+  );
+}
---
+++ frontend/src/components/Modals/MangeWorkspace/Documents/Directory/index.jsx
@@ -0,0 +1,310 @@
+import UploadFile from "../UploadFile";
+import PreLoader from "../../../../Preloader";
+import { useEffect, useRef, useState } from "react";
+import { CaretDown, File, FolderNotch, Trash } from "@phosphor-icons/react";
+
+export default function Directory({
+  files,
+  parent = null,
+  nested = 0,
+  loading,
+}) {
+    const [selectedItems, setSelectedItems] = useState({});
+
+    const toggleSelection = (item) => {
+        setSelectedItems((prevSelectedItems) => {
+          // Copy the previous selection state.
+          const newSelectedItems = { ...prevSelectedItems };
+
+          if (item.type === 'folder') {
+            if (newSelectedItems[item.id]) {
+              delete newSelectedItems[item.id]; // Deselect the folder.
+              item.items.forEach(file => delete newSelectedItems[file.id]); // Deselect all files in the folder.
+            } else {
+              newSelectedItems[item.id] = true; // Select the folder.
+              item.items.forEach(file => newSelectedItems[file.id] = true); // Select all files in the folder.
+            }
+          } else { // For files.
+            if (newSelectedItems[item.id]) {
+              delete newSelectedItems[item.id]; // Deselect the file.
+            } else {
+              newSelectedItems[item.id] = true; // Select the file.
+            }
+          }
+
+          return newSelectedItems;
+        });
+      };
+
+  useEffect(() => {
+    console.log("SELECTED ITEMS: ", selectedItems);
+    }, [selectedItems]);
+
+
+    const isSelected = (id) => {
+        return !!selectedItems[id];
+      };
+
+  const onTrashClick = (event) => {
+    event.stopPropagation();
+    console.log("trash clicked");
+  };
+
+  if (loading) {
+    return (
+      <div className="px-8 pb-8">
+        <div className="flex flex-col gap-y-6">
+          <div className="flex items-center justify-between w-[560px] px-5">
+            <h3 className="text-white text-base font-bold">My Documents</h3>
+          </div>
+
+          <div className="w-[560px] h-[310px] bg-zinc-900 rounded-2xl overflow-y-auto relative">
+            <div className="text-white/80 text-xs grid grid-cols-12 py-2 px-8 border-b border-white/20 shadow-lg bg-zinc-900 sticky top-0">
+              <p className="col-span-4">Name</p>
+              <p className="col-span-2">Date</p>
+              <p className="col-span-2">Size</p>
+              <p className="col-span-2">Kind</p>
+              <p className="col-span-2">Cached</p>
+            </div>
+            <div className="w-full h-full flex items-center justify-center">
+              <PreLoader />
+            </div>
+          </div>
+
+          <UploadFile />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="px-8 pb-8">
+      <div className="flex flex-col gap-y-6">
+        <div className="flex items-center justify-between w-[560px] px-5">
+          <h3 className="text-white text-base font-bold">My Documents</h3>
+        </div>
+
+        <div className="w-[560px] h-[310px] bg-zinc-900 rounded-2xl overflow-y-auto relative">
+          <div className="text-white/80 text-xs grid grid-cols-12 py-2 px-8 border-b border-white/20 shadow-lg bg-zinc-900 sticky top-0">
+            <p className="col-span-4">Name</p>
+            <p className="col-span-2">Date</p>
+            <p className="col-span-2">Size</p>
+            <p className="col-span-2">Kind</p>
+            <p className="col-span-2">Cached</p>
+          </div>
+          {/* .items is each item */}
+          {!!files.items ? (
+            // .item is each file's contents split
+            files.items.map((item, index) =>
+              // Render folders and files here
+              item.type === "folder" ? (
+                //   <p>folder</p>
+                <FolderRow
+                  key={index}
+                  item={item}
+                  selected={isSelected(item.id)}
+                  onRowClick={() => toggleSelection(item)}
+                  toggleSelection={toggleSelection}
+                  onTrashClick={onTrashClick}
+                />
+              ) : (
+                <p>file</p>
+              )
+            )
+          ) : (
+            <div className="w-full h-full flex items-center justify-center">
+              <p className="text-white text-opacity-40 text-sm font-medium">
+                No Documents
+              </p>
+            </div>
+          )}
+        </div>
+
+        <UploadFile />
+      </div>
+    </div>
+  );
+}
+
+function FolderRow({
+  item,
+  selected,
+  onRowClick,
+  onTrashClick,
+  toggleSelection,
+}) {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = (event) => {
+    event.stopPropagation();
+    setExpanded(!expanded);
+  };
+
+  console.log("FOLDER ROW ITEM IN FOLDER ROW: ", item);
+
+  return (
+    <>
+      <div
+        onClick={onRowClick}
+        className={`transition-all duration-200 text-white/80 text-xs grid grid-cols-12 py-2 pl-3.5 pr-8 border-b border-white/20 hover:bg-sky-500/20 cursor-pointer w-full ${
+          selected ? "bg-sky-500/20" : ""
+        }`}
+      >
+        <div className="col-span-4 flex gap-x-[4px] items-center">
+          {/* Checkbox */}
+          <div
+            className="w-3 h-3 rounded border-[1px] border-white flex justify-center items-center cursor-pointer"
+            role="checkbox"
+            aria-checked={selected}
+            tabIndex={0}
+          >
+            {selected && <div className="w-2 h-2 bg-white rounded-[2px]" />}
+          </div>
+          <div
+            onClick={handleExpandClick}
+            className={`transform transition-transform duration-200 ${
+              expanded ? "rotate-360" : " rotate-270"
+            }`}
+          >
+            <CaretDown className="text-base font-bold w-4 h-4" />
+          </div>
+          <FolderNotch
+            className="text-base font-bold w-4 h-4 mr-[3px]"
+            weight="fill"
+          />
+          <p className="whitespace-nowrap overflow-hidden">
+            {truncate(item.name, 20)}
+          </p>
+        </div>
+        <p className="col-span-2 pl-3.5">---</p>
+        <p className="col-span-2 pl-3">{item.size}</p>
+        <p className="col-span-2 pl-2">{document.kind}</p>
+        <div className="col-span-2 flex justify-end items-center">
+          {item.cached && (
+            <div className="bg-white/10 rounded-3xl">
+              <p className="text-xs px-2 py-0.5">Cached</p>
+            </div>
+          )}
+          <Trash
+            onClick={onTrashClick}
+            className="text-base font-bold w-4 h-4 ml-2 flex-shrink-0 cursor-pointer"
+          />
+        </div>
+      </div>
+      {expanded && (
+        <div className="col-span-full">
+          {item.items.map((item) => (
+            <FileRow
+              key={item.id}
+              item={item}
+              selected={selected}
+              expanded={expanded}
+              onTrashClick={onTrashClick}
+              onRowClick={() => toggleSelection(item)}
+            />
+          ))}
+        </div>
+      )}
+    </>
+  );
+}
+
+function FileRow({ item, selected, onRowClick, onTrashClick, expanded }) {
+  const [showTooltip, setShowTooltip] = useState(false);
+  const tooltipTimeoutRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    tooltipTimeoutRef.current = setTimeout(() => {
+      setShowTooltip(true);
+    }, 300);
+  };
+
+  const handleMouseLeave = () => {
+    clearTimeout(tooltipTimeoutRef.current);
+    setShowTooltip(false);
+  };
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(tooltipTimeoutRef.current);
+    };
+  }, []);
+
+  return (
+    <div
+      onClick={onRowClick}
+      className={`transition-all duration-200 text-white/80 text-xs grid grid-cols-12 py-2 pl-3.5 pr-8 border-b border-white/20 hover:bg-sky-500/20 cursor-pointer ${`${
+        selected ? "bg-sky-500/20" : ""
+      } ${expanded ? "bg-sky-500/10" : ""}`}`}
+    >
+      <div className="col-span-4 flex gap-x-[4px] items-center">
+        {/* Checkbox */}
+        <div
+          className="w-3 h-3 rounded border-[1px] border-white flex justify-center items-center cursor-pointer"
+          role="checkbox"
+          aria-checked={selected}
+          tabIndex={0}
+        >
+          {selected && <div className="w-2 h-2 bg-white rounded-[2px]" />}
+        </div>
+        <File className="text-base font-bold w-4 h-4 mr-[3px]" weight="fill" />
+        <div
+          className="relative"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <p className="whitespace-nowrap overflow-hidden">
+            {truncate(item.name, 17)}
+          </p>
+          {showTooltip && (
+            <div className="absolute left-0 bg-white text-black p-1.5 rounded shadow-lg whitespace-nowrap">
+              {item.name}
+            </div>
+          )}
+        </div>
+      </div>
+      <p className="col-span-2 pl-3.5 whitespace-nowrap">
+        {formatDate(item?.published)}
+      </p>
+      <p className="col-span-2 pl-3">{item?.size || "---"}</p>
+      <p className="col-span-2 pl-2">{item?.type}</p>
+      <div className="col-span-2 flex justify-end items-center">
+        {item?.cached && (
+          <div className="bg-white/10 rounded-3xl">
+            <p className="text-xs px-2 py-0.5">Cached</p>
+          </div>
+        )}
+        <Trash
+          onClick={onTrashClick}
+          className="text-base font-bold w-4 h-4 ml-2 flex-shrink-0 cursor-pointer"
+        />
+      </div>
+    </div>
+  );
+}
+
+function truncate(str, n) {
+  const fileExtensionPattern = /(\..+)$/;
+  const extensionMatch = str.match(fileExtensionPattern);
+
+  if (extensionMatch && extensionMatch[1]) {
+    const extension = extensionMatch[1];
+    const nameWithoutExtension = str.replace(fileExtensionPattern, "");
+    const truncationPoint = Math.max(0, n - extension.length - 4);
+    const truncatedName =
+      nameWithoutExtension.substr(0, truncationPoint) +
+      "..." +
+      nameWithoutExtension.slice(-4);
+
+    return truncatedName + extension;
+  } else {
+    return str.length > n ? str.substr(0, n - 8) + "..." + str.slice(-4) : str;
+  }
+}
+
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const options = { year: "numeric", month: "short", day: "numeric" };
+  const formattedDate = date.toLocaleDateString("en-US", options);
+  return formattedDate;
+};
