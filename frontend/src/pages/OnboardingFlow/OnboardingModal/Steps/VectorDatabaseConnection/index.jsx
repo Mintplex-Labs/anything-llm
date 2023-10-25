5d20bf0312c64c7966ad4c47dc0678e0d703fd17
shatfield4 <seanhatfield5@gmail.com>
2023-10-24 17:10:28 -0700

    fix onboarding flow vector db settings to save to db

--- frontend/src/pages/OnboardingFlow/OnboardingModal/Steps/VectorDatabaseConnection/index.jsx
+++ frontend/src/pages/OnboardingFlow/OnboardingModal/Steps/VectorDatabaseConnection/index.jsx
@@ -18,7 +18,7 @@ function VectorDatabaseConnection({ nextStep, prevStep, currentStep }) {
     async function fetchKeys() {
       const _settings = await System.keys();
       setSettings(_settings);
-      setVectorDB(_settings?.VectorDB || "lancedb");
+      setVectorDB(_settings?.vectorDB || "lancedb");
       setLoading(false);
     }
     if (currentStep === 2) {
@@ -60,7 +60,7 @@ function VectorDatabaseConnection({ nextStep, prevStep, currentStep }) {
             Select your preferred vector database provider
           </div>
           <div className="w-full flex md:flex-wrap overflow-x-scroll gap-4 max-w-[900px]">
-            <input hidden={true} name="VectorDB" value={vectorDB} />
+            <input hidden={true} name="vector_db" value={vectorDB} />
             <VectorDBOption
               name="Chroma"
               value="chroma"
@@ -116,7 +116,7 @@ function VectorDatabaseConnection({ nextStep, prevStep, currentStep }) {
                   </label>
                   <input
                     type="password"
-                    name="PineConeKey"
+                    name="pinecone_api_key"
                     className="bg-zinc-900 text-white placeholder-white placeholder-opacity-60 text-sm rounded-lg focus:border-white block w-full p-2.5"
                     placeholder="Pinecone API Key"
                     defaultValue={settings?.PineConeKey ? "*".repeat(20) : ""}
@@ -132,7 +132,7 @@ function VectorDatabaseConnection({ nextStep, prevStep, currentStep }) {
                   </label>
                   <input
                     type="text"
-                    name="PineConeEnvironment"
+                    name="pinecone_environment"
                     className="bg-zinc-900 text-white placeholder-white placeholder-opacity-60 text-sm rounded-lg focus:border-white block w-full p-2.5"
                     placeholder="us-gcp-west-1"
                     defaultValue={settings?.PineConeEnvironment}
@@ -148,7 +148,7 @@ function VectorDatabaseConnection({ nextStep, prevStep, currentStep }) {
                   </label>
                   <input
                     type="text"
-                    name="PineConeIndex"
+                    name="pinecone_index"
                     className="bg-zinc-900 text-white placeholder-white placeholder-opacity-60 text-sm rounded-lg focus:border-white block w-full p-2.5"
                     placeholder="my-index"
                     defaultValue={settings?.PineConeIndex}
@@ -168,7 +168,7 @@ function VectorDatabaseConnection({ nextStep, prevStep, currentStep }) {
                   </label>
                   <input
                     type="url"
-                    name="ChromaEndpoint"
+                    name="chroma_endpoint"
                     className="bg-zinc-900 text-white placeholder-white placeholder-opacity-60 text-sm rounded-lg focus:border-white block w-full p-2.5"
                     placeholder="http://localhost:8000"
                     defaultValue={settings?.ChromaEndpoint}
@@ -183,7 +183,7 @@ function VectorDatabaseConnection({ nextStep, prevStep, currentStep }) {
                     API Header
                   </label>
                   <input
-                    name="ChromaApiHeader"
+                    name="chroma_api_header"
                     autoComplete="off"
                     type="text"
                     defaultValue={settings?.ChromaApiHeader}
@@ -197,7 +197,7 @@ function VectorDatabaseConnection({ nextStep, prevStep, currentStep }) {
                     API Key
                   </label>
                   <input
-                    name="ChromaApiKey"
+                    name="chroma_api_key"
                     autoComplete="off"
                     type="password"
                     defaultValue={settings?.ChromaApiKey ? "*".repeat(20) : ""}
@@ -224,7 +224,7 @@ function VectorDatabaseConnection({ nextStep, prevStep, currentStep }) {
                   </label>
                   <input
                     type="url"
-                    name="QdrantEndpoint"
+                    name="qdrant_endpoint"
                     className="bg-zinc-900 text-white placeholder-white placeholder-opacity-60 text-sm rounded-lg focus:border-white block w-full p-2.5"
                     placeholder="http://localhost:6633"
                     defaultValue={settings?.QdrantEndpoint}
@@ -240,7 +240,7 @@ function VectorDatabaseConnection({ nextStep, prevStep, currentStep }) {
                   </label>
                   <input
                     type="password"
-                    name="QdrantApiKey"
+                    name="qdrant_api_key"
                     className="bg-zinc-900 text-white placeholder-white placeholder-opacity-60 text-sm rounded-lg focus:border-white block w-full p-2.5"
                     placeholder="wOeqxsYP4....1244sba"
                     defaultValue={settings?.QdrantApiKey}
@@ -259,7 +259,7 @@ function VectorDatabaseConnection({ nextStep, prevStep, currentStep }) {
                   </label>
                   <input
                     type="url"
-                    name="WeaviateEndpoint"
+                    name="weaviate_endpoint"
                     className="bg-zinc-900 text-white placeholder-white placeholder-opacity-60 text-sm rounded-lg focus:border-white block w-full p-2.5"
                     placeholder="http://localhost:8080"
                     defaultValue={settings?.WeaviateEndpoint}
@@ -275,7 +275,7 @@ function VectorDatabaseConnection({ nextStep, prevStep, currentStep }) {
                   </label>
                   <input
                     type="password"
-                    name="WeaviateApiKey"
+                    name="weaviate_api_key"
                     className="bg-zinc-900 text-white placeholder-white placeholder-opacity-60 text-sm rounded-lg focus:border-white block w-full p-2.5"
                     placeholder="sk-123Abcweaviate"
                     defaultValue={settings?.WeaviateApiKey}
