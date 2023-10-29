01cc3053efacf19756e0143df427ed358fade54f
timothycarambat <rambat1010@gmail.com>
2023-10-29 10:39:23 -0700

    WIP aws fixes to latest changes;

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-node /app/server/index.js &
+{ cd /app/server/ &&\
+  npx prisma generate --schema=./prisma/schema.prisma &&\
+  npx prisma migrate deploy --schema=./prisma/schema.prisma &&\
+  node /app/server/index.js
+} &
 { FLASK_ENV=production FLASK_APP=wsgi.py cd collector && gunicorn --timeout 300 --workers 4 --bind 0.0.0.0:8888 wsgi:api; } &
 wait -n
 exit $?
