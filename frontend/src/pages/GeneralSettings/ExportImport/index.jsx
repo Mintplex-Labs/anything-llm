469659ebf507c3df5a447d030256b6b9c424497c
timothycarambat <rambat1010@gmail.com>
2023-11-06 13:22:31 -0800

    Update UI on disabled import/export

--- frontend/src/pages/GeneralSettings/ExportImport/index.jsx
+++ frontend/src/pages/GeneralSettings/ExportImport/index.jsx
@@ -8,8 +8,48 @@ import showToast from "../../../utils/toast";
 import { CloudArrowUp, DownloadSimple } from "@phosphor-icons/react";
 import System from "../../../models/system";
 import { API_BASE } from "../../../utils/constants";
+import paths from "../../../utils/paths";
 
 export default function GeneralExportImport() {
+  const hostname = window?.location?.hostname;
+  const isHosted = hostname.includes(".useanything.com");
+
+  if (isHosted) {
+    return (
+      <div className="w-screen h-screen overflow-hidden bg-sidebar flex">
+        {!isMobile && <Sidebar />}
+        <div
+          style={{ height: isMobile ? "100%" : "calc(100% - 32px)" }}
+          className="transition-all duration-500 relative md:ml-[2px] md:mr-[8px] md:my-[16px] md:rounded-[26px] bg-main-gradient md:min-w-[82%] p-[18px] h-full overflow-y-scroll"
+        >
+          {isMobile && <SidebarMobileHeader />}
+          <div className="flex flex-col w-full px-1 md:px-20 md:py-12 py-16">
+            <div className="w-full flex flex-col gap-y-1 pb-6 border-white border-b-2 border-opacity-10">
+              <div className="items-center flex gap-x-4">
+                <p className="text-2xl font-semibold text-white">
+                  Export or Import
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full items-center justify-center flex flex-col gap-y-4">
+            <p className="text-lg font-base text-white text-opacity-60">
+              This feature is temporarily disabled for hosted AnythingLLM
+              instances.
+            </p>
+            <a
+              href={`${paths.mailToMintplex()}?Subject=Import/Export disabled on hosted AnythingLLM.`}
+              className="text-blue-300 hover:underline"
+            >
+              Contact Mintplex Labs Inc.
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="w-screen h-screen overflow-hidden bg-sidebar flex">
       {!isMobile && <Sidebar />}
@@ -47,12 +87,7 @@ function ImportData() {
   const [file, setFile] = useState(null);
   const [result, setResult] = useState(null);
 
-  // Disable import on hosted instances
-  const hostname = window.location.hostname;
-  const isHosted = hostname.includes(".useanything.com");
-
   const startInput = () => inputRef?.current?.click();
-
   const handleUpload = async (e) => {
     setLoading(true);
     e.preventDefault();
@@ -80,19 +115,6 @@ function ImportData() {
     setLoading(false);
     setFile(null);
   };
-
-  if (isHosted) {
-    return (
-      <div className="max-w-[600px] py-4 bg-zinc-900/50 rounded-2xl border-2 border-dashed border-white border-opacity-60 justify-center items-center inline-flex transition-all duration-300 hover:opacity-60 cursor-not-allowed">
-        <div className="flex flex-col items-center justify-center">
-          <CloudArrowUp className="w-8 h-8 text-white/80" />
-          <div className="text-white text-opacity-80 text-sm font-semibold py-1">
-            Importing is currently unavailable on hosted instances.
-          </div>
-        </div>
-      </div>
-    );
-  }
 
   return (
     <div
