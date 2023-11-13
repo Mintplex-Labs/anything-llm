0c1702c1d2dbd68492efee0e74371a3e500e0371
timothycarambat <rambat1010@gmail.com>
2023-11-13 14:29:01 -0800

    update render conditional

--- frontend/src/pages/Admin/Users/UserRow/index.jsx
+++ frontend/src/pages/Admin/Users/UserRow/index.jsx
@@ -40,7 +40,7 @@ export default function UserRow({ currUser, user }) {
         <td className="px-6 py-4">{titleCase(user.role)}</td>
         <td className="px-6 py-4">{user.createdAt}</td>
         <td className="px-6 py-4 flex items-center gap-x-6">
-          {!(currUser?.role === "manager" && user.role === "admin") && (
+          {currUser?.role !== "default" && (
             <button
               onClick={() =>
                 document?.getElementById(EditUserModalId(user))?.showModal()
@@ -50,23 +50,22 @@ export default function UserRow({ currUser, user }) {
               <DotsThreeOutline weight="fill" className="h-5 w-5" />
             </button>
           )}
-          {currUser?.id !== user.id &&
-            !(currUser?.role === "manager" && user.role === "admin") && (
-              <>
-                <button
-                  onClick={handleSuspend}
-                  className="font-medium text-orange-600 dark:text-orange-300 px-2 py-1 rounded-lg hover:bg-orange-50 hover:dark:bg-orange-800 hover:dark:bg-opacity-20"
-                >
-                  {suspended ? "Unsuspend" : "Suspend"}
-                </button>
-                <button
-                  onClick={handleDelete}
-                  className="font-medium text-red-600 dark:text-red-300 px-2 py-1 rounded-lg hover:bg-red-50 hover:dark:bg-red-800 hover:dark:bg-opacity-20"
-                >
-                  Delete
-                </button>
-              </>
-            )}
+          {currUser?.id !== user.id && currUser?.role !== "default" && (
+            <>
+              <button
+                onClick={handleSuspend}
+                className="font-medium text-orange-600 dark:text-orange-300 px-2 py-1 rounded-lg hover:bg-orange-50 hover:dark:bg-orange-800 hover:dark:bg-opacity-20"
+              >
+                {suspended ? "Unsuspend" : "Suspend"}
+              </button>
+              <button
+                onClick={handleDelete}
+                className="font-medium text-red-600 dark:text-red-300 px-2 py-1 rounded-lg hover:bg-red-50 hover:dark:bg-red-800 hover:dark:bg-opacity-20"
+              >
+                Delete
+              </button>
+            </>
+          )}
         </td>
       </tr>
       <EditUserModal currentUser={currUser} user={user} />
