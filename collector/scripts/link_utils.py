92a0d19fac4cb1aab05cdd2673ab69865aec20fe
shatfield4 <seanhatfield5@gmail.com>
2023-11-14 12:56:52 -0800

    fix manual script for uploading url to custom-documents

--- collector/scripts/link_utils.py
+++ collector/scripts/link_utils.py
@@ -1,11 +1,13 @@
 import json
 from datetime import datetime
+from .watch.utils import guid
 from dotenv import load_dotenv
 load_dotenv()
 
 def append_meta(request, text, metadata_only = False):
   meta = {
     'url': request.url,
+    'id': guid(),
     'title': request.html.find('title', first=True).text if len(request.html.find('title')) != 0 else '',
     'description': request.html.find('meta[name="description"]', first=True).attrs.get('content') if  request.html.find('meta[name="description"]', first=True) != None else '',
     'published':request.html.find('meta[property="article:published_time"]', first=True).attrs.get('content') if request.html.find('meta[property="article:published_time"]', first=True) != None else datetime.today().strftime('%Y-%m-%d %H:%M:%S'),
