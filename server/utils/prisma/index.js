26dc168c77837dee1ced4655df41e568e2a64d25
shatfield4 <seanhatfield5@gmail.com>
2023-11-13 16:58:17 -0800

    linting

--- server/utils/prisma/index.js
+++ server/utils/prisma/index.js
@@ -5,9 +5,9 @@ const { PrismaClient } = require("@prisma/client");
 // npx prisma migrate dev --name init -> ensures that db is in sync with schema
 // npx prisma migrate reset -> resets the db
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 const prisma = new PrismaClient({
-  log: isProd ? ['error'] : ['query', 'info', 'warn'],
+  log: isProd ? ["error"] : ["query", "info", "warn"],
 });
 
 module.exports = prisma;
