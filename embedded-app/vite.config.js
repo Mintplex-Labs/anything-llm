cc8b886e14397bccb467940777f9c11422d6cbbc
shatfield4 <seanhatfield5@gmail.com>
2023-11-17 17:22:45 -0800

    create embedded vite app that compiles down to single js file/hosts on yarn serve

---
+++ embedded-app/vite.config.js
@@ -0,0 +1,32 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import image from '@rollup/plugin-image';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    image()
+  ],
+  define: { // In dev, we need to disable this, but in prod, we need to enable it
+    'process.env.NODE_ENV': JSON.stringify('production'),
+  },
+  build: {
+    lib: {
+      entry: 'src/main.jsx',
+      name: 'EmbeddedAnythingLLM',
+      formats: ['umd'],
+      fileName: (format) => `embedded-anything-llm.${format}.js`
+    },
+    rollupOptions: {
+      external: [],
+    },
+    cssCodeSplit: false,
+    assetsInlineLimit: 100000000,
+    minify: 'esbuild',
+    outDir: 'dist',
+    emptyOutDir: true,
+    inlineDynamicImports: true,
+    assetsDir: '',
+  }
+});
