6a0e0cc6ba90dc9ed7701fd2e576313137e31ac5
Paul Ritchey <pritchey@gmail.com>
2023-11-29 08:17:07 -0500

    Added improved password complexity checking capability.

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,44 @@
 const prisma = require("../utils/prisma");
 const bcrypt = require("bcrypt");
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
+
+function checkPasswordComplexity(password) {
+  // Set defaults for password complexity or use user defined settings.
+  const complexityOptions = {
+    min: process.env.PASSWORDMINCHAR || 8,
+    max: process.env.PASSWORDMAXCHAR || 250,
+    lowerCase: process.env.PASSWORDLOWERCASE || 1,
+    upperCase: process.env.PASSWORDUPPERCASE || 0,
+    numeric: process.env.PASSWORDNUMERIC || 0,
+    symbol: process.env.PASSWORDSYMBOL || 0,
+    requirementCount: process.env.PASSWORDREQUIREMENTS || 1,
+  };
+
+  let complexityCheck = passwordComplexity(complexityOptions, 'password').validate(password);
+
+  // Check if password passed complexity check, if it did not
+  // gather all of the missed checks into one error string.
+  if (complexityCheck.hasOwnProperty('error')) {
+    let myError = "";
+    let prepend = "";
+    for (let i = 0; i < complexityCheck.error.details.length; i++) {
+      myError += prepend + complexityCheck.error.details[i].message;
+      prepend = ", ";
+    }
+    return { 'checkedOK': false, 'error': myError };
+  }
+  return { 'checkedOK': true, 'error': 'No error.' };
+}
 
 const User = {
   create: async function ({ username, password, role = "default" }) {
+    // Ensure password meets complexity requirements.
+    passwordCheck = checkPasswordComplexity(password);
+    if (!passwordCheck.checkedOK) {
+      return { success: false, error: passwordCheck.error };
+    }
+
     try {
       const hashedPassword = bcrypt.hashSync(password, 10);
       const user = await prisma.users.create({
@@ -20,6 +56,12 @@ const User = {
   },
 
   update: async function (userId, updates = {}) {
+    // Ensure password meets complexity requirements.
+    passwordCheck = checkPasswordComplexity(updates["password"]);
+    if (!passwordCheck.checkedOK) {
+      return { success: false, error: passwordCheck.error };
+    }
+
     try {
       // Rehash new password if it exists as update
       // will be given to us as plaintext.
