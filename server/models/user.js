3338c5d6fdd158a692a6db4cb21a10872a5dc234
timothycarambat <rambat1010@gmail.com>
2023-12-05 08:59:41 -0800

    Move password complexity checker as User.util dynamically import required libraries depending on code execution flow lint

--- server/models/user.js
+++ server/models/user.js
@@ -1,45 +1,14 @@
 const prisma = require("../utils/prisma");
-const bcrypt = require("bcrypt");
-const Joi = require("joi");
-const passwordComplexity = require("joi-password-complexity");
-
-function checkPasswordComplexity(password) {
-  // Set defaults for password complexity or use user defined settings.
-  const complexityOptions = {
-    min: process.env.PASSWORDMINCHAR || 8,
-    max: process.env.PASSWORDMAXCHAR || 250,
-    lowerCase: process.env.PASSWORDLOWERCASE || 1,
-    upperCase: process.env.PASSWORDUPPERCASE || 0,
-    numeric: process.env.PASSWORDNUMERIC || 0,
-    symbol: process.env.PASSWORDSYMBOL || 0,
-    requirementCount: process.env.PASSWORDREQUIREMENTS || 1,
-  };
-
-  let complexityCheck = passwordComplexity(complexityOptions, 'password').validate(password);
-
-  // Check if password passed complexity check, if it did not
-  // gather all of the missed checks into one error string.
-  if (complexityCheck.hasOwnProperty('error')) {
-    let myError = "";
-    let prepend = "";
-    for (let i = 0; i < complexityCheck.error.details.length; i++) {
-      myError += prepend + complexityCheck.error.details[i].message;
-      prepend = ", ";
-    }
-    return { 'checkedOK': false, 'error': myError };
-  }
-  return { 'checkedOK': true, 'error': 'No error.' };
-}
 
 const User = {
   create: async function ({ username, password, role = "default" }) {
-    // Ensure password meets complexity requirements.
-    passwordCheck = checkPasswordComplexity(password);
+    const passwordCheck = this.checkPasswordComplexity(password);
     if (!passwordCheck.checkedOK) {
-      return { success: false, error: passwordCheck.error };
+      return { user: null, error: passwordCheck.error };
     }
 
     try {
+      const bcrypt = require("bcrypt");
       const hashedPassword = bcrypt.hashSync(password, 10);
       const user = await prisma.users.create({
         data: {
@@ -56,16 +25,15 @@ const User = {
   },
 
   update: async function (userId, updates = {}) {
-    // Ensure password meets complexity requirements.
-    passwordCheck = checkPasswordComplexity(updates["password"]);
-    if (!passwordCheck.checkedOK) {
-      return { success: false, error: passwordCheck.error };
-    }
-
     try {
-      // Rehash new password if it exists as update
-      // will be given to us as plaintext.
-      if (updates.hasOwnProperty("password") && updates.password.length >= 8) {
+      // Rehash new password if it exists as update field
+      if (updates.hasOwnProperty("password")) {
+        const passwordCheck = this.checkPasswordComplexity(updates.password);
+        if (!passwordCheck.checkedOK) {
+          return { success: false, error: passwordCheck.error };
+        }
+
+        const bcrypt = require("bcrypt");
         updates.password = bcrypt.hashSync(updates.password, 10);
       } else {
         delete updates.password;
@@ -123,6 +91,38 @@ const User = {
       console.error(error.message);
       return [];
     }
+  },
+
+  checkPasswordComplexity: function (passwordInput = "") {
+    const passwordComplexity = require("joi-password-complexity");
+    // Can be set via ENV variable on boot. No frontend config at this time.
+    // Docs: https://www.npmjs.com/package/joi-password-complexity
+    const complexityOptions = {
+      min: process.env.PASSWORDMINCHAR || 8,
+      max: process.env.PASSWORDMAXCHAR || 250,
+      lowerCase: process.env.PASSWORDLOWERCASE || 0,
+      upperCase: process.env.PASSWORDUPPERCASE || 0,
+      numeric: process.env.PASSWORDNUMERIC || 0,
+      symbol: process.env.PASSWORDSYMBOL || 0,
+      // reqCount should be equal to how many conditions you are testing for (1-4)
+      requirementCount: process.env.PASSWORDREQUIREMENTS || 0,
+    };
+
+    const complexityCheck = passwordComplexity(
+      complexityOptions,
+      "password"
+    ).validate(passwordInput);
+    if (complexityCheck.hasOwnProperty("error")) {
+      let myError = "";
+      let prepend = "";
+      for (let i = 0; i < complexityCheck.error.details.length; i++) {
+        myError += prepend + complexityCheck.error.details[i].message;
+        prepend = ", ";
+      }
+      return { checkedOK: false, error: myError };
+    }
+
+    return { checkedOK: true, error: "No error." };
   },
 };
 
