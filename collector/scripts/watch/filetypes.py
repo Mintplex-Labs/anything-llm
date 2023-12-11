ce9233c258a6769ddf4e640dd49623c5c7b0f37d
Timothy Carambat <rambat1010@gmail.com>
2023-12-11 14:40:33 -0800

    feat: enable HTML uploads from UI (#422)
    resolves #418

--- collector/scripts/watch/filetypes.py
+++ collector/scripts/watch/filetypes.py
@@ -3,6 +3,7 @@ from .convert.as_markdown import as_markdown
 from .convert.as_pdf import as_pdf
 from .convert.as_docx import as_docx, as_odt
 from .convert.as_mbox import as_mbox
+from .convert.as_html import as_html
 
 FILETYPES = {
     '.txt': as_text,
@@ -11,10 +12,12 @@ FILETYPES = {
     '.docx': as_docx,
     '.odt': as_odt,
     '.mbox': as_mbox, 
+    '.html': as_html, 
 }
 
 ACCEPTED_MIMES = {
     'text/plain': ['.txt', '.md'],
+    'text/html': ['.html'], 
     'application/vnd.openxmlformats-officedocument.wordprocessingml.document': ['.docx'],
     'application/vnd.oasis.opendocument.text': ['.odt'],
     'application/pdf': ['.pdf'],
