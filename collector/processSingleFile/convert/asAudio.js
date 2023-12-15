51675f9e9031018684a233eaa4aab00b8e0c8e7a
timothycarambat <rambat1010@gmail.com>
2023-12-15 10:09:52 -0800

    additional logging

--- collector/processSingleFile/convert/asAudio.js
+++ collector/processSingleFile/convert/asAudio.js
@@ -14,8 +14,17 @@ async function asAudio({ fullFilePath = "", filename = "" }) {
   const whisper = new LocalWhisper();
 
   console.log(`-- Working ${filename} --`);
-  const transcriber = await whisper.client();
   const audioData = await convertToWavAudioData(fullFilePath);
+  if (!audioData) {
+    console.error(`Failed to parse content from ${filename}.`);
+    trashFile(fullFilePath);
+    return {
+      success: false,
+      reason: `Failed to parse content from ${filename}.`,
+    };
+  }
+
+  const transcriber = await whisper.client();
   const { text: content } = await transcriber(audioData, {
     chunk_length_s: 30,
     stride_length_s: 5,
@@ -50,59 +59,71 @@ async function asAudio({ fullFilePath = "", filename = "" }) {
 }
 
 async function convertToWavAudioData(sourcePath) {
-  const wavefile = require("wavefile");
-  const ffmpeg = require("fluent-ffmpeg");
-  const outFolder = path.resolve(__dirname, `../../storage/tmp`);
-  let buffer;
-  if (!fs.existsSync(outFolder)) fs.mkdirSync(outFolder, { recursive: true });
+  try {
+    let buffer;
+    const wavefile = require("wavefile");
+    const ffmpeg = require("fluent-ffmpeg");
+    const outFolder = path.resolve(__dirname, `../../storage/tmp`);
+    if (!fs.existsSync(outFolder)) fs.mkdirSync(outFolder, { recursive: true });
 
-  const fileExtension = path.extname(sourcePath).toLowerCase();
-  if (fileExtension !== ".wav") {
-    console.log(`${fileExtension} file detected - converting to .wav`);
-    const outputFile = path.resolve(outFolder, `${v4()}.wav`);
-    const convert = new Promise((resolve) => {
-      ffmpeg(sourcePath)
-        .toFormat("wav")
-        .on("error", (err) => {
-          console.log("An error occurred: " + err.message);
-          resolve(false);
-        })
-        .on("progress", (progress) => {
-          console.log("Processing: " + progress.targetSize + " KB converted");
-        })
-        .on("end", () => {
-          console.log("Processing finished !");
-          resolve(true);
-        })
-        .save(outputFile);
-    });
-    const success = await convert;
-    if (!success) throw new Error("Could not convert file to parsable format!");
-    buffer = Buffer.from(fs.readFileSync(outputFile));
-    fs.rmSync(outputFile);
-  } else {
-    buffer = Buffer.from(fs.readFileSync(sourcePath));
-  }
-
-  const wavFile = new wavefile.WaveFile(buffer);
-  wavFile.toBitDepth("32f");
-  wavFile.toSampleRate(16000);
-
-  let audioData = wavFile.getSamples();
-  if (Array.isArray(audioData)) {
-    if (audioData.length > 1) {
-      const SCALING_FACTOR = Math.sqrt(2);
-
-      // Merge channels (into first channel to save memory)
-      for (let i = 0; i < audioData[0].length; ++i) {
-        audioData[0][i] =
-          (SCALING_FACTOR * (audioData[0][i] + audioData[1][i])) / 2;
-      }
+    const fileExtension = path.extname(sourcePath).toLowerCase();
+    if (fileExtension !== ".wav") {
+      console.log(
+        `[Conversion Required] ${fileExtension} file detected - converting to .wav`
+      );
+      const outputFile = path.resolve(outFolder, `${v4()}.wav`);
+      const convert = new Promise((resolve) => {
+        ffmpeg(sourcePath)
+          .toFormat("wav")
+          .on("error", (error) => {
+            console.error(`[Conversion Error] ${error.message}`);
+            resolve(false);
+          })
+          .on("progress", (progress) =>
+            console.log(
+              `[Conversion Processing]: ${progress.targetSize}KB converted`
+            )
+          )
+          .on("end", () => {
+            console.log("[Conversion Complete]: File converted to .wav!");
+            resolve(true);
+          })
+          .save(outputFile);
+      });
+      const success = await convert;
+      if (!success)
+        throw new Error(
+          "[Conversion Failed]: Could not convert file to .wav format!"
+        );
+      buffer = Buffer.from(fs.readFileSync(outputFile));
+      fs.rmSync(outputFile);
+    } else {
+      buffer = Buffer.from(fs.readFileSync(sourcePath));
     }
-    audioData = audioData[0];
-  }
 
-  return audioData;
+    const wavFile = new wavefile.WaveFile(buffer);
+    wavFile.toBitDepth("32f");
+    wavFile.toSampleRate(16000);
+
+    let audioData = wavFile.getSamples();
+    if (Array.isArray(audioData)) {
+      if (audioData.length > 1) {
+        const SCALING_FACTOR = Math.sqrt(2);
+
+        // Merge channels into first channel to save memory
+        for (let i = 0; i < audioData[0].length; ++i) {
+          audioData[0][i] =
+            (SCALING_FACTOR * (audioData[0][i] + audioData[1][i])) / 2;
+        }
+      }
+      audioData = audioData[0];
+    }
+
+    return audioData;
+  } catch (error) {
+    console.error(`convertToWavAudioData`, error);
+    return null;
+  }
 }
 
 module.exports = asAudio;
