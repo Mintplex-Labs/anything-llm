545542cd93e7a766b0f83da420247c3c28bb6961
shatfield4 <seanhatfield5@gmail.com>
2024-01-02 16:20:01 -0800

    fix survey width in onboarding

--- frontend/src/pages/OnboardingFlow/Steps/Survey/index.jsx
+++ frontend/src/pages/OnboardingFlow/Steps/Survey/index.jsx
@@ -29,61 +29,66 @@ export default function Survey({ setHeader, setForwardBtn }) {
   }
 
   return (
-    <form onSubmit={handleSubmit} ref={formRef}>
-      <div>
-        <label htmlFor="nameInput" className="text-white text-base font-medium">
-          What's your name?{" "}
-          <span className="text-neutral-400 text-base font-light">
-            (Optional)
-          </span>
-        </label>
-        <input
-          id="nameInput"
-          type="text"
-          placeholder="Enter name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="mt-2 bg-zinc-900 text-white text-sm font-medium font-['Plus Jakarta Sans'] leading-tight w-full h-11 p-2.5 bg-zinc-900 rounded-lg"
-        />
-      </div>
-
-      <div className="mt-8">
-        <label className="text-white text-base font-medium">
-          What will you use AnythingLLM for?{" "}
-          <span className="text-neutral-400 text-base font-light">
-            (Optional)
-          </span>
-        </label>
-        <div className="mt-2 gap-y-3 flex flex-col">
-          {options.map((option) => (
-            <label
-              key={option}
-              className={`transition-all duration-300 w-full h-11 p-2.5 bg-white/10 rounded-lg flex justify-start items-center gap-2.5 cursor-pointer border border-transparent ${
-                selectedOption === option
-                  ? "border-white border-opacity-40"
-                  : ""
-              } hover:border-white/60`}
-            >
-              <input
-                type="radio"
-                name="usageOption"
-                value={option}
-                checked={selectedOption === option}
-                onChange={(e) => setSelectedOption(e.target.value)}
-                className="hidden"
-              />
-              <div
-                className={`w-4 h-4 rounded-full border-2 border-white mr-2 ${
-                  selectedOption === option ? "bg-white" : ""
-                }`}
-              ></div>
-              <div className="text-white text-sm font-medium font-['Plus Jakarta Sans'] leading-tight">
-                {option}
-              </div>
-            </label>
-          ))}
+    <div className="w-full flex justify-center">
+      <form onSubmit={handleSubmit} ref={formRef} className="">
+        <div>
+          <label
+            htmlFor="nameInput"
+            className="text-white text-base font-medium"
+          >
+            What's your name?{" "}
+            <span className="text-neutral-400 text-base font-light">
+              (Optional)
+            </span>
+          </label>
+          <input
+            id="nameInput"
+            type="text"
+            placeholder="Enter name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="mt-2 bg-zinc-900 text-white text-sm font-medium font-['Plus Jakarta Sans'] leading-tight w-full h-11 p-2.5 bg-zinc-900 rounded-lg"
+          />
         </div>
-      </div>
-    </form>
+
+        <div className="mt-8">
+          <label className="text-white text-base font-medium">
+            What will you use AnythingLLM for?{" "}
+            <span className="text-neutral-400 text-base font-light">
+              (Optional)
+            </span>
+          </label>
+          <div className="mt-2 gap-y-3 flex flex-col">
+            {options.map((option) => (
+              <label
+                key={option}
+                className={`transition-all duration-300 w-full h-11 p-2.5 bg-white/10 rounded-lg flex justify-start items-center gap-2.5 cursor-pointer border border-transparent ${
+                  selectedOption === option
+                    ? "border-white border-opacity-40"
+                    : ""
+                } hover:border-white/60`}
+              >
+                <input
+                  type="radio"
+                  name="usageOption"
+                  value={option}
+                  checked={selectedOption === option}
+                  onChange={(e) => setSelectedOption(e.target.value)}
+                  className="hidden"
+                />
+                <div
+                  className={`w-4 h-4 rounded-full border-2 border-white mr-2 ${
+                    selectedOption === option ? "bg-white" : ""
+                  }`}
+                ></div>
+                <div className="text-white text-sm font-medium font-['Plus Jakarta Sans'] leading-tight">
+                  {option}
+                </div>
+              </label>
+            ))}
+          </div>
+        </div>
+      </form>
+    </div>
   );
 }
