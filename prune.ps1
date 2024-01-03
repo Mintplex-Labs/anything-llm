6d84535d3f506f5800550a72f405847904fe76ff
Timothy Carambat <rambat1010@gmail.com>
2024-01-03 12:36:20 -0800

    update powershell script for exact match only

--- prune.ps1
+++ prune.ps1
@@ -1,16 +1,25 @@
 Write-Host "Removing useless files from dist-server/node_modules."
 
 # Common unneeded files
+$unneededFileNames = @(
+    "Makefile", "README", "README.md", "CHANGELOG", "CHANGELOG.md", ".editorconfig", ".gitmodules", ".gitattributes", "robot.html", 
+    ".lint", "Gulpfile.js", "Gruntfile.js", ".tern-project", ".gitattributes", ".editorconfig", ".eslintrc", ".jshintrc", 
+    ".npmignore", ".flowconfig", ".documentup.json", ".yarn-metadata.json", ".travis.yml", "thumbs.db", ".tern-port", 
+    ".ds_store", "desktop.ini", "npm-debug.log", ".npmrc", "LICENSE.txt", "LICENSE.md", "LICENSE-MIT", "LICENSE-MIT.txt", 
+    "LICENSE.BSD", "LICENSE-BSD", "LICENSE-jsbn", "LICENSE", "AUTHORS", "CONTRIBUTORS", ".yarn-integrity", "builderror.log", 
+    "*.md", "*.sln", "*.obj", "*.gypi", "*.vcxproj", "*.vcxproj.filters"
+)
+
 Get-ChildItem -Path .\ -Filter node_modules -Recurse -Directory | ForEach-Object {
     Get-ChildItem -Path $_.FullName -File -Recurse | Where-Object {
-        $_.Name -match "README|README.md|CHANGELOG|CHANGELOG.md|.editorconfig|.gitmodules|.gitattributes|robot.html|.lint|Gulpfile.js|Gruntfile.js|.tern-project|.gitattributes|.editorconfig|.eslintrc|.jshintrc|.npmignore|.flowconfig|.documentup.json|.yarn-metadata.json|.travis.yml|thumbs.db|.tern-port|.ds_store|desktop.ini|npm-debug.log|.npmrc|LICENSE.txt|LICENSE.md|LICENSE-MIT|LICENSE-MIT.txt|LICENSE.BSD|LICENSE-BSD|LICENSE-jsbn|LICENSE|AUTHORS|CONTRIBUTORS|.yarn-integrity|builderror.log|.md|.sln|.obj|.gypi|.vcxproj|.vcxproj.filters|\.ts$" -and $_.Name -notmatch '\.d\.ts$' -and $_.Name -notmatch '\.jst|\.coffee'
+        $_.Name -in $unneededFileNames -and $_.Name -notmatch '\.d\.ts$' -and $_.Name -notmatch '\.jst|\.coffee'
     } | Remove-Item -Force -Recurse
 }
 
 # Common unneeded directories
 Get-ChildItem -Path .\ -Filter node_modules -Recurse -Directory | ForEach-Object {
     Get-ChildItem -Path $_.FullName -Directory -Recurse | Where-Object {
-        $_.Name -match "__tests__|test|tests|powered-test|docs|doc|website|images|assets|example|examples|coverage|.nyc_output"
+        $_.Name -in @("__tests__", "test", "tests", "powered-test", "docs", "doc", "website", "images", "assets", "example", "examples", "coverage", ".nyc_output")
     } | Remove-Item -Force -Recurse
 }
 
