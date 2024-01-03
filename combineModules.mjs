0e130b14bb5f123f5160acf87704e7228adc2706
Timothy Carambat <rambat1010@gmail.com>
2024-01-03 10:49:10 -0800

    windows support

--- combineModules.mjs
+++ combineModules.mjs
@@ -1,4 +1,4 @@
-import fs, { rmSync } from 'fs';
+import fs from 'fs';
 import { join } from 'path';
 import serverInfo from './server/package.json' assert {type: "json"}
 import collectorInfo from './collector/package.json' assert {type: "json"}
@@ -14,7 +14,15 @@ fs.mkdirSync(outFolder);
 const mergedDeps = Object.assign({}, serverDeps, collectorDeps);
 fs.writeFileSync(join(outFolder, 'package.json'), JSON.stringify({ dependencies: mergedDeps }, null, 2), 'utf8');
 execSync(`cd ${outFolder} && yarn install --prod=true`, { stdio: 'inherit' });
-execSync(`cp ./prune.sh ${outFolder}/prune.sh && cd ${outFolder} && bash prune.sh`, { stdio: 'inherit' });
-rmSync(`${outFolder}/package.json`)
-rmSync(`${outFolder}/yarn.lock`)
-rmSync(`${outFolder}/prune.sh`)
+
+if(process.platform === 'win32') {
+    fs.copyFileSync('prune.ps1', `${outFolder}/prune.ps1`);
+    execSync(`@powershell -NoProfile -ExecutionPolicy Unrestricted -Command ${outFolder}/prune.ps1`, { stdio: 'inherit' });
+    fs.rmSync(`${outFolder}/prune.ps1`)
+} else {
+    execSync(`cp ./prune.sh ${outFolder}/prune.sh && cd ${outFolder} && bash prune.sh`, { stdio: 'inherit' });
+    fs.rmSync(`${outFolder}/prune.sh`)
+}
+
+fs.rmSync(`${outFolder}/package.json`)
+fs.rmSync(`${outFolder}/yarn.lock`)
