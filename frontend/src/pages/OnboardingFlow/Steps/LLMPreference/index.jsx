038a294d5b2824360457bc3c3ba463e2b794e74f
shatfield4 <seanhatfield5@gmail.com>
2024-01-03 16:58:55 -0800

    LLM Preference screen fully functional

--- frontend/src/pages/OnboardingFlow/Steps/LLMPreference/index.jsx
+++ frontend/src/pages/OnboardingFlow/Steps/LLMPreference/index.jsx
@@ -1,5 +1,5 @@
 import { MagnifyingGlass } from "@phosphor-icons/react";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 const TITLE = "LLM Preference";
 const DESCRIPTION =
   "AnythingLLM can work with many LLM providers. This will be the service which handles chatting.";
@@ -22,6 +22,7 @@ import OllamaLLMOptions from "@/components/LLMSelection/OllamaLLMOptions";
 import LLMItem from "./LLMItem";
 import System from "@/models/system";
 import paths from "@/utils/paths";
+import showToast from "@/utils/toast";
 
 export default function LLMPreference({
   setHeader,
@@ -32,6 +33,9 @@ export default function LLMPreference({
   const [filteredLLMs, setFilteredLLMs] = useState([]);
   const [selectedLLM, setSelectedLLM] = useState(null);
   const [settings, setSettings] = useState(null);
+  const formRef = useRef(null);
+  const hiddenSubmitButtonRef = useRef(null);
+  const isHosted = window.location.hostname.includes("useanything.com");
 
   useEffect(() => {
     async function fetchKeys() {
@@ -104,7 +108,9 @@ export default function LLMPreference({
   ];
 
   function handleForward() {
-    window.location.href = paths.onboarding.vectorDatabase();
+    if (hiddenSubmitButtonRef.current) {
+      hiddenSubmitButtonRef.current.click();
+    }
   }
 
   function handleBack() {
@@ -113,15 +119,20 @@ export default function LLMPreference({
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const data = { LLMProvider: selectedLLM, ...settings };
-    console.log(data);
-    // const { error } = await System.updateSystem(data);
-    // if (error) {
-    //   alert(`Failed to save LLM settings: ${error}`, "error");
-    //   return;
-    // }
-    // console.log("LLM settings saved successfully.");
-    // Navigate to next step or handle the continuation logic here
+    const form = e.target;
+    const data = {};
+    const formData = new FormData(form);
+    data.LLMProvider = selectedLLM;
+    for (var [key, value] of formData.entries()) data[key] = value;
+
+    const { error } = await System.updateSystem(data);
+    if (error) {
+      showToast(`Failed to save LLM settings: ${error}`, "error");
+      return;
+    }
+    showToast("LLM settings saved successfully.", "success");
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    window.location.href = paths.onboarding.vectorDatabase();
   };
 
   useEffect(() => {
@@ -143,47 +154,57 @@ export default function LLMPreference({
   }, [searchQuery]);
 
   return (
-    <>
-      <form onSubmit={handleSubmit} className="w-full">
+    <div>
+      <form ref={formRef} onSubmit={handleSubmit} className="w-full">
         <div className="w-full border-slate-300/40 shadow border-2 rounded-lg p-4 text-white overflow-y-auto">
           <div className="flex items-center sticky top-0 z-20">
             <MagnifyingGlass
               size={16}
               weight="bold"
-              className="absolute left-4 z-10 text-white"
+              className="absolute left-4 z-30 text-white"
             />
             <input
               type="text"
               placeholder="Search LLM Providers"
-              className="bg-white/10 pl-10 rounded-full w-full px-4 py-1 text-sm border-2 border-slate-300/40 outline-none focus:border-white text-white z-30"
+              className="bg-zinc-600 z-20 pl-10 rounded-full w-full px-4 py-1 text-sm border-2 border-slate-300/40 outline-none focus:border-white text-white"
               onChange={(e) => setSearchQuery(e.target.value)}
               autoComplete="off"
+              onKeyDown={(e) => {
+                if (e.key === "Enter") e.preventDefault();
+              }}
             />
           </div>
           <div className="mt-4 flex flex-col gap-y-1 max-h-[390px]">
-            {filteredLLMs.map((llm) => (
-              <LLMItem
-                key={llm.name}
-                name={llm.name}
-                value={llm.value}
-                image={llm.logo}
-                description={llm.description}
-                checked={selectedLLM === llm.value}
-                onClick={() => setSelectedLLM(llm.value)}
-              />
-            ))}
+            {filteredLLMs.map((llm) => {
+              if (llm.value === "native" && isHosted) {
+                return null;
+              }
+
+              return (
+                <LLMItem
+                  key={llm.name}
+                  name={llm.name}
+                  value={llm.value}
+                  image={llm.logo}
+                  description={llm.description}
+                  checked={selectedLLM === llm.value}
+                  onClick={() => setSelectedLLM(llm.value)}
+                />
+              );
+            })}
           </div>
         </div>
         <div className="mt-4 flex flex-col gap-y-1">
           {selectedLLM &&
             LLMS.find((llm) => llm.value === selectedLLM)?.options}
         </div>
-        <div className="flex justify-between items-center px-6 py-4">
-          <button type="submit" className="submit-button-styles">
-            Save
-          </button>
-        </div>
+        <button
+          type="submit"
+          ref={hiddenSubmitButtonRef}
+          hidden
+          aria-hidden="true"
+        ></button>
       </form>
-    </>
+    </div>
   );
 }
