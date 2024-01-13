4f6d93159f686500c5e4c5ab60256496de1a18e6
Timothy Carambat <rambat1010@gmail.com>
2024-01-13 00:32:43 -0800

    improve native embedder handling of large files (#584)
    * improve native embedder handling of large files
    
    * perf changes
    
    * ignore storage tmp

--- server/utils/EmbeddingEngines/native/index.js
+++ server/utils/EmbeddingEngines/native/index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 const { toChunks } = require("../../helpers");
+const { v4 } = require("uuid");
 
 class NativeEmbedder {
   constructor() {
@@ -15,11 +16,28 @@ class NativeEmbedder {
     this.dimensions = 384;
 
     // Limit of how many strings we can process in a single pass to stay with resource or network limits
-    this.maxConcurrentChunks = 50;
+    this.maxConcurrentChunks = 25;
     this.embeddingMaxChunkLength = 1_000;
 
     // Make directory when it does not exist in existing installations
     if (!fs.existsSync(this.cacheDir)) fs.mkdirSync(this.cacheDir);
+  }
+
+  #tempfilePath() {
+    const filename = `${v4()}.tmp`;
+    const tmpPath = process.env.STORAGE_DIR
+      ? path.resolve(process.env.STORAGE_DIR, "tmp")
+      : path.resolve(__dirname, `../../../storage/tmp`);
+    if (!fs.existsSync(tmpPath)) fs.mkdirSync(tmpPath, { recursive: true });
+    return path.resolve(tmpPath, filename);
+  }
+
+  async #writeToTempfile(filePath, data) {
+    try {
+      await fs.promises.appendFile(filePath, data, { encoding: "utf8" });
+    } catch (e) {
+      console.error(`Error writing to tempfile: ${e}`);
+    }
   }
 
   async embedderClient() {
@@ -62,18 +80,51 @@ class NativeEmbedder {
     return result?.[0] || [];
   }
 
+  // If you are thinking you want to edit this function - you probably don't.
+  // This process was benchmarked heavily on a t3.small (2GB RAM 1vCPU)
+  // and without careful memory management for the V8 garbage collector
+  // this function will likely result in an OOM on any resource-constrained deployment.
+  // To help manage very large documents we run a concurrent write-log each iteration
+  // to keep the embedding result out of memory. The `maxConcurrentChunk` is set to 25,
+  // as 50 seems to overflow no matter what. Given the above, memory use hovers around ~30%
+  // during a very large document (>100K words) but can spike up to 70% before gc.
+  // This seems repeatable for all document sizes.
+  // While this does take a while, it is zero set up and is 100% free and on-instance.
   async embedChunks(textChunks = []) {
-    const Embedder = await this.embedderClient();
-    const embeddingResults = [];
-    for (const chunk of toChunks(textChunks, this.maxConcurrentChunks)) {
-      const output = await Embedder(chunk, {
+    const tmpFilePath = this.#tempfilePath();
+    const chunks = toChunks(textChunks, this.maxConcurrentChunks);
+    const chunkLen = chunks.length;
+
+    for (let [idx, chunk] of chunks.entries()) {
+      if (idx === 0) await this.#writeToTempfile(tmpFilePath, "[");
+      let data;
+      let pipeline = await this.embedderClient();
+      let output = await pipeline(chunk, {
         pooling: "mean",
         normalize: true,
       });
-      if (output.length === 0) continue;
-      embeddingResults.push(output.tolist());
+
+      if (output.length === 0) {
+        pipeline = null;
+        output = null;
+        data = null;
+        continue;
+      }
+
+      data = JSON.stringify(output.tolist());
+      await this.#writeToTempfile(tmpFilePath, data);
+      console.log(`\x1b[34m[Embedded Chunk ${idx + 1} of ${chunkLen}]\x1b[0m`);
+      if (chunkLen - 1 !== idx) await this.#writeToTempfile(tmpFilePath, ",");
+      if (chunkLen - 1 === idx) await this.#writeToTempfile(tmpFilePath, "]");
+      pipeline = null;
+      output = null;
+      data = null;
     }
 
+    const embeddingResults = JSON.parse(
+      fs.readFileSync(tmpFilePath, { encoding: "utf-8" })
+    );
+    fs.rmSync(tmpFilePath, { force: true });
     return embeddingResults.length > 0 ? embeddingResults.flat() : null;
   }
 }
