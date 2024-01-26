0066f047cde690bcf2fd5530198a017504f70129
shatfield4 <seanhatfield5@gmail.com>
2024-01-26 12:56:08 -0800

    add gpt-4-turbo-preview to valid models

--- server/utils/AiProviders/openAi/index.js
+++ server/utils/AiProviders/openAi/index.js
@@ -67,6 +67,7 @@ class OpenAiLLM {
       "gpt-3.5-turbo",
       "gpt-3.5-turbo-1106",
       "gpt-4-1106-preview",
+      "gpt-4-turbo-preview",
       "gpt-4-32k",
     ];
     const isPreset = validModels.some((model) => modelName === model);
