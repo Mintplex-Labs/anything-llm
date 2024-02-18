1739e2ac1bc6dc7a874800f8922388dd14256cf8
timothycarambat <rambat1010@gmail.com>
2024-02-17 16:52:08 -0800

    small refactor

--- frontend/src/pages/GeneralSettings/Appearance/SupportEmail/index.jsx
+++ frontend/src/pages/GeneralSettings/Appearance/SupportEmail/index.jsx
@@ -1,28 +1,36 @@
+import useUser from "@/hooks/useUser";
 import Admin from "@/models/admin";
 import System from "@/models/system";
 import showToast from "@/utils/toast";
 import { useEffect, useState } from "react";
 
 export default function SupportEmail() {
+  const { user } = useUser();
   const [loading, setLoading] = useState(true);
   const [hasChanges, setHasChanges] = useState(false);
   const [supportEmail, setSupportEmail] = useState("");
+  const [originalEmail, setOriginalEmail] = useState("");
 
   useEffect(() => {
     const fetchSupportEmail = async () => {
       const supportEmail = await System.fetchSupportEmail();
       setSupportEmail(supportEmail.email || "");
+      setOriginalEmail(supportEmail.email || "");
       setLoading(false);
     };
     fetchSupportEmail();
   }, []);
 
-  const updateSupportEmail = async (e) => {
+  const updateSupportEmail = async (e, newValue = null) => {
     e.preventDefault();
-    const form = new FormData(e.target);
-    const supportEmail = form.get("supportEmail");
+    let support_email = newValue;
+    if (newValue === null) {
+      const form = new FormData(e.target);
+      support_email = form.get("supportEmail");
+    }
+
     const { success, error } = await Admin.updateSystemPreferences({
-      support_email: supportEmail,
+      support_email,
     });
 
     if (!success) {
@@ -31,66 +39,48 @@ export default function SupportEmail() {
     } else {
       showToast("Successfully updated support email.", "success");
       window.localStorage.removeItem(System.cacheKeys.supportEmail);
+      setSupportEmail(support_email);
+      setOriginalEmail(support_email);
       setHasChanges(false);
     }
   };
 
-  const removeEmail = async (e) => {
-    e.preventDefault();
-    const { success, error } = await Admin.updateSystemPreferences({
-      support_email: null,
-    });
-
-    if (!success) {
-      showToast(`Failed to remove support email: ${error}`, "error");
-      return;
-    } else {
-      setSupportEmail("");
-      showToast("Successfully removed support email.", "success");
-      window.localStorage.removeItem(System.cacheKeys.supportEmail);
-      setHasChanges(false);
-    }
-  };
-
-  const onEmailChange = (e) => {
+  const handleChange = (e) => {
     setSupportEmail(e.target.value);
     setHasChanges(true);
   };
 
-  if (loading) return null;
-
+  if (loading || !user?.role) return null;
   return (
     <form className="mb-6" onSubmit={updateSupportEmail}>
       <div className="flex flex-col gap-y-2">
         <h2 className="leading-tight font-medium text-white">Support Email</h2>
         <p className="text-sm font-base text-white/60">
-          Set the support email address that shows up in the user menu.
+          Set the support email address that shows up in the user menu while
+          logged into this instance.
         </p>
       </div>
-      <div className="flex items-center gap-x-6">
+      <div className="flex items-center gap-x-4">
         <input
           name="supportEmail"
           type="email"
-          minLength={2}
-          maxLength={80}
           className="bg-zinc-900 mt-4 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 max-w-[275px]"
           placeholder="support@mycompany.com"
           required={true}
           autoComplete="off"
-          onChange={onEmailChange}
+          onChange={handleChange}
           value={supportEmail}
         />
-        {supportEmail !== "" && (
+        {originalEmail !== "" && (
           <button
             type="button"
-            onClick={removeEmail}
-            className="text-white text-base font-medium hover:text-opacity-60"
+            onClick={(e) => updateSupportEmail(e, "")}
+            className="mt-4 text-white text-base font-medium hover:text-opacity-60"
           >
-            Remove
+            Clear
           </button>
         )}
       </div>
-
       {hasChanges && (
         <button
           type="submit"
