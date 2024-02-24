f8a49f605069b9806d4070490822c10ed89ace30
timothycarambat <rambat1010@gmail.com>
2024-02-23 18:00:51 -0800

    linting

--- frontend/src/utils/chat/markdown.js
+++ frontend/src/utils/chat/markdown.js
@@ -26,7 +26,7 @@ const markdown = markdownIt({
           hljs.highlight(code, { language: lang, ignoreIllegals: true }).value +
           "</pre></div>"
         );
-      } catch (__) { }
+      } catch (__) {}
     }
 
     return (
