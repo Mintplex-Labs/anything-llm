42a082563e935d315766cc84eb752de0262c42a9
shatfield4 <seanhatfield5@gmail.com>
2024-02-29 13:50:55 -0800

    WIP footer customization new UI

--- frontend/src/pages/GeneralSettings/Appearance/FooterCustomization/index.jsx
+++ frontend/src/pages/GeneralSettings/Appearance/FooterCustomization/index.jsx
@@ -11,6 +11,7 @@ export default function FooterCustomization() {
   const [loading, setLoading] = useState(true);
   const [footerIcons, setFooterIcons] = useState([]);
   const [showForm, setShowForm] = useState(false);
+  console.log(footerIcons);
 
   useEffect(() => {
     async function fetchFooterIcons() {
@@ -40,32 +41,6 @@ export default function FooterCustomization() {
     setFooterIcons(updatedIcons);
     showToast("Successfully removed footer icon.", "success", { clear: true });
   };
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    const form = new FormData(e.target);
-    const icon = form.get("icon");
-    const url = form.get("url");
-
-    const newIcon = { icon, url };
-    setFooterIcons([...footerIcons, newIcon]);
-
-    const { success, error } = await Admin.updateSystemPreferences({
-      footer_data: JSON.stringify([...footerIcons, newIcon]),
-    });
-
-    if (!success) {
-      showToast(`Failed to add footer icon - ${error}`, "error", {
-        clear: true,
-      });
-      return;
-    }
-    window.localStorage.removeItem(System.cacheKeys.footerIcons);
-
-    setShowForm(false);
-    showToast("Successfully added footer icon.", "success", { clear: true });
-  };
-
   return (
     <div className="mb-8">
       <div className="flex flex-col gap-y-1">
@@ -76,25 +51,148 @@ export default function FooterCustomization() {
           Customize the footer icons displayed on the bottom of the sidebar.
         </p>
       </div>
-      <CurrentIcons footerIcons={footerIcons} remove={removeFooterIcon} />
-      <NewIconForm
-        handleSubmit={onSubmit}
-        showing={footerIcons.length < MAX_ICONS && showForm}
-      />
-      <div hidden={!(!showForm && footerIcons.length < MAX_ICONS) || loading}>
-        <div className="flex gap-2 mt-6">
-          <button
-            onClick={() => setShowForm(true)}
-            className="flex gap-x-2 items-center justify-center text-white text-sm hover:text-sky-400 transition-all duration-300"
-          >
-            Add new footer icon
-            <Plus className="" size={24} weight="fill" />
+      <div className="mt-3 flex gap-x-3 font-bold text-white text-sm">
+        <div>Icon</div>
+        <div>Link</div>
+      </div>
+      <div className="mt-2 flex flex-col gap-y-[10px]">
+        <div className="flex items-center gap-x-1.5">
+          <div className="h-[34px] w-[34px] bg-[#1C1E21] rounded-full flex items-center justify-center">
+            {" "}
+            {React.createElement(ICON_COMPONENTS["DiscordLogo"], {
+              className: "h-5 w-5 text-white",
+              weight: "fill",
+            })}
+          </div>
+          <input
+            type="url"
+            name="url"
+            required={true}
+            placeholder="https://example.com"
+            className="bg-zinc-900 text-white placeholder-white/20 text-sm rounded-md p-2.5 w-[300px] h-[32px]"
+          />
+          <button onClick={() => setShowForm(true)}>
+            <X
+              className="transition-all duration-300 text-white bg-transparent rounded-full hover:bg-zinc-600 hover:border-zinc-600 hover:text-white border-transparent border shadow-lg"
+              size={20}
+              weight="bold"
+            />
           </button>
+        </div>
+
+        <div className="flex items-center gap-x-1.5">
+          <div className="h-[34px] w-[34px] bg-[#1C1E21] rounded-full"></div>
+          <input
+            type="url"
+            name="url"
+            required={true}
+            placeholder="https://example.com"
+            className="bg-zinc-900 text-white placeholder-white/20 text-sm rounded-md p-2.5 w-[300px] h-[32px]"
+          />
+        </div>
+
+        <div className="flex items-center gap-x-1.5">
+          <div className="h-[34px] w-[34px] bg-[#1C1E21] rounded-full"></div>
+          <input
+            type="url"
+            name="url"
+            required={true}
+            placeholder="https://example.com"
+            className="bg-zinc-900 text-white placeholder-white/20 text-sm rounded-md p-2.5 w-[300px] h-[32px]"
+          />
         </div>
       </div>
     </div>
   );
 }
+// export default function FooterCustomization() {
+//   const [loading, setLoading] = useState(true);
+//   const [footerIcons, setFooterIcons] = useState([]);
+//   const [showForm, setShowForm] = useState(false);
+
+//   useEffect(() => {
+//     async function fetchFooterIcons() {
+//       const settings = (await Admin.systemPreferences())?.settings;
+//       if (settings && settings.footer_data) {
+//         setFooterIcons(safeJsonParse(settings.footer_data, []));
+//       }
+//       setLoading(false);
+//     }
+//     fetchFooterIcons();
+//   }, []);
+
+//   const removeFooterIcon = async (index) => {
+//     const updatedIcons = footerIcons.filter((_, i) => i !== index);
+//     const { success, error } = await Admin.updateSystemPreferences({
+//       footer_data: JSON.stringify(updatedIcons),
+//     });
+
+//     if (!success) {
+//       showToast(`Failed to remove footer icon - ${error}`, "error", {
+//         clear: true,
+//       });
+//       return;
+//     }
+
+//     window.localStorage.removeItem(System.cacheKeys.footerIcons);
+//     setFooterIcons(updatedIcons);
+//     showToast("Successfully removed footer icon.", "success", { clear: true });
+//   };
+
+//   const onSubmit = async (e) => {
+//     e.preventDefault();
+//     const form = new FormData(e.target);
+//     const icon = form.get("icon");
+//     const url = form.get("url");
+
+//     const newIcon = { icon, url };
+//     setFooterIcons([...footerIcons, newIcon]);
+
+//     const { success, error } = await Admin.updateSystemPreferences({
+//       footer_data: JSON.stringify([...footerIcons, newIcon]),
+//     });
+
+//     if (!success) {
+//       showToast(`Failed to add footer icon - ${error}`, "error", {
+//         clear: true,
+//       });
+//       return;
+//     }
+//     window.localStorage.removeItem(System.cacheKeys.footerIcons);
+
+//     setShowForm(false);
+//     showToast("Successfully added footer icon.", "success", { clear: true });
+//   };
+
+//   return (
+//     <div className="mb-8">
+//       <div className="flex flex-col gap-y-1">
+//         <h2 className="text-base leading-6 font-bold text-white">
+//           Custom Footer Icons
+//         </h2>
+//         <p className="text-xs leading-[18px] font-base text-white/60">
+//           Customize the footer icons displayed on the bottom of the sidebar.
+//         </p>
+//       </div>
+//       <CurrentIcons footerIcons={footerIcons} remove={removeFooterIcon} />
+//       <NewIconForm
+//         handleSubmit={onSubmit}
+//         showing={footerIcons.length < MAX_ICONS && showForm}
+//       />
+//       <div hidden={!(!showForm && footerIcons.length < MAX_ICONS) || loading}>
+//         <div className="flex gap-2 mt-6">
+//           <button
+//             onClick={() => setShowForm(true)}
+//             className="flex gap-x-2 items-center justify-center text-white text-sm hover:text-sky-400 transition-all duration-300"
+//           >
+//             Add new footer icon
+//             <Plus className="" size={24} weight="fill" />
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
 function CurrentIcons({ footerIcons, remove }) {
   if (footerIcons.length === 0) return null;
