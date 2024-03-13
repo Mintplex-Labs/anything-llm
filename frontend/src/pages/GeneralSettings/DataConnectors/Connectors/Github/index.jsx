e4424a8e3dcc3506127f25623811f5dbb570b6c7
timothycarambat <rambat1010@gmail.com>
2024-03-12 17:35:20 -0700

    fix desktop view size for data connectors

--- frontend/src/pages/GeneralSettings/DataConnectors/Connectors/Github/index.jsx
+++ frontend/src/pages/GeneralSettings/DataConnectors/Connectors/Github/index.jsx
@@ -63,11 +63,11 @@ export default function GithubConnectorSetup() {
 
   return (
     <div
-      style={{ height: "calc(100vh - 32px)" }}
+      style={{ height: "calc(100vh - 40px)" }}
       className="w-screen overflow-hidden bg-sidebar flex"
     >
       <Sidebar />
-      <div className="relative md:ml-[2px] md:mr-[16px] md:my-[16px] md:rounded-[16px] bg-main-gradient w-full h-full overflow-y-scroll border-4 border-accent">
+      <div className="transition-all duration-500 relative ml-[2px] mr-[16px] my-[16px] md:rounded-[26px] bg-main-gradient w-full h-[93vh] overflow-y-scroll border-4 border-accent">
         <div className="flex w-full">
           <div className="flex flex-col w-full px-1 md:pl-6 md:pr-[86px] md:py-6 py-16">
             <div className="flex w-full gap-x-4 items-center pb-6 border-white border-b-2 border-opacity-10">
