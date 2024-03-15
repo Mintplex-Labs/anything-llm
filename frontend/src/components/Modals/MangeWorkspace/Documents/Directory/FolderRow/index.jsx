a6d15b5ebfeb19911daeb04656d61776eb2d710d
shatfield4 <seanhatfield5@gmail.com>
2024-03-15 15:06:04 -0700

    update alternating row css for file picker

--- frontend/src/components/Modals/MangeWorkspace/Documents/Directory/FolderRow/index.jsx
+++ frontend/src/components/Modals/MangeWorkspace/Documents/Directory/FolderRow/index.jsx
@@ -25,7 +25,7 @@ export default function FolderRow({
     <>
       <tr
         onClick={onRowClick}
-        className={`transition-all duration-200 text-white/80 text-xs grid grid-cols-12 py-2 pl-3.5 pr-8 bg-[#2C2C2C] hover:bg-sky-500/20 cursor-pointer w-full file-row ${
+        className={`transition-all duration-200 text-white/80 text-xs grid grid-cols-12 py-2 pl-3.5 pr-8 bg-[#1C1E21] hover:bg-sky-500/20 cursor-pointer w-full ${
           selected ? "selected" : ""
         }`}
       >
