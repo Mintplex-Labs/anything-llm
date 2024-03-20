00cbe0ac7a1885f244875c319be0d4ef9c9a04c9
sherifButt <90522472+sherifButt@users.noreply.github.com>
2024-03-20 00:49:18 +0000

    fixed bad import resulted in error

--- frontend/src/pages/WorkspaceSettings/MetaResponse/InputsFeature/index.jsx
+++ frontend/src/pages/WorkspaceSettings/MetaResponse/InputsFeature/index.jsx
@@ -1,16 +1,18 @@
 import TextAreaBlock from "@/components/Generic/Blocks/TextAreaBlock";
 import EnableSystemPrompt from "./EnableSystemPrompt";
-import { C } from "../../../../../dist/assets/index-cf7f0eac";
 import CheckBoxBlock from "@/components/Generic/Blocks/CheckBoxBlock";
 
 export default function InputsFeature({ workspace, config }) {
-  console.log("workspace: ", workspace);
-  console.log("config: ", config);
   return (
     <div className="flex flex-col gap-4 mt-4">
-      <EnableSystemPrompt workspace={workspace} config={config} />
+      <EnableSystemPrompt workspace={workspace} config={config}  />
       <TextAreaBlock workspace={workspace} />
-      <CheckBoxBlock  workspace={workspace}  label="override workspace prompt" inline name="systemPrompt"/>
+      <CheckBoxBlock
+        workspace={workspace}
+        label="override workspace prompt"
+        inline
+        name="systemPrompt"
+      />
     </div>
   );
 }
