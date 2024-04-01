a8017bb6a3d3587414ea457d3732225b39ffeb1f
shatfield4 <seanhatfield5@gmail.com>
2024-04-01 12:01:40 -0700

    implement new UI for embedder preference onboarding

--- frontend/src/pages/OnboardingFlow/Steps/EmbeddingPreference/index.jsx
+++ frontend/src/pages/OnboardingFlow/Steps/EmbeddingPreference/index.jsx
@@ -1,4 +1,4 @@
-import { MagnifyingGlass } from "@phosphor-icons/react";
+import { CaretUpDown, MagnifyingGlass, X } from "@phosphor-icons/react";
 import { useEffect, useState, useRef } from "react";
 import AnythingLLMIcon from "@/media/logo/anything-llm-icon.png";
 import OpenAiLogo from "@/media/llmprovider/openai.png";
@@ -25,22 +25,79 @@ export default function EmbeddingPreference({
   setForwardBtn,
   setBackBtn,
 }) {
+  const [settings, setSettings] = useState(null);
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredEmbedders, setFilteredEmbedders] = useState([]);
   const [selectedEmbedder, setSelectedEmbedder] = useState(null);
-  const [settings, setSettings] = useState(null);
+  const [searchMenuOpen, setSearchMenuOpen] = useState(true);
+  const searchInputRef = useRef(null);
   const formRef = useRef(null);
   const hiddenSubmitButtonRef = useRef(null);
   const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const data = {};
+    const formData = new FormData(form);
+    data.EmbeddingEngine = selectedEmbedder;
+    for (var [key, value] of formData.entries()) data[key] = value;
+
+    const { error } = await System.updateSystem(data);
+    if (error) {
+      showToast(`Failed to save embedding settings: ${error}`, "error");
+      return;
+    }
+    navigate(paths.onboarding.vectorDatabase());
+  };
+
+  function handleForward() {
+    if (hiddenSubmitButtonRef.current) {
+      hiddenSubmitButtonRef.current.click();
+    }
+  }
+
+  function handleBack() {
+    navigate(paths.onboarding.llmPreference());
+  }
+
+  useEffect(() => {
+    setHeader({ title: TITLE, description: DESCRIPTION });
+    setForwardBtn({ showing: true, disabled: false, onClick: handleForward });
+    setBackBtn({ showing: true, disabled: false, onClick: handleBack });
+  }, []);
+
+  const updateEmbedderChoice = (selection) => {
+    setSearchQuery("");
+    setSelectedEmbedder(selection);
+    setSearchMenuOpen(false);
+  };
+
+  const handleXButton = () => {
+    if (searchQuery.length > 0) {
+      setSearchQuery("");
+      if (searchInputRef.current) searchInputRef.current.value = "";
+    } else {
+      setSearchMenuOpen(!searchMenuOpen);
+    }
+  };
 
   useEffect(() => {
     async function fetchKeys() {
       const _settings = await System.keys();
       setSettings(_settings);
       setSelectedEmbedder(_settings?.EmbeddingEngine || "native");
+      console.log(_settings?.EmbeddingEngine);
     }
     fetchKeys();
   }, []);
+
+  useEffect(() => {
+    const filtered = EMBEDDERS.filter((embedder) =>
+      embedder.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredEmbedders(filtered);
+  }, [searchQuery, selectedEmbedder]);
 
   const EMBEDDERS = [
     {
@@ -81,96 +138,94 @@ export default function EmbeddingPreference({
     },
   ];
 
-  function handleForward() {
-    if (hiddenSubmitButtonRef.current) {
-      hiddenSubmitButtonRef.current.click();
-    }
-  }
-
-  function handleBack() {
-    navigate(paths.onboarding.llmPreference());
-  }
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const data = {};
-    const formData = new FormData(form);
-    data.EmbeddingEngine = selectedEmbedder;
-    for (var [key, value] of formData.entries()) data[key] = value;
-
-    const { error } = await System.updateSystem(data);
-    if (error) {
-      showToast(`Failed to save embedding settings: ${error}`, "error");
-      return;
-    }
-    navigate(paths.onboarding.vectorDatabase());
-  };
-
-  useEffect(() => {
-    setHeader({ title: TITLE, description: DESCRIPTION });
-    setForwardBtn({ showing: true, disabled: false, onClick: handleForward });
-    setBackBtn({ showing: true, disabled: false, onClick: handleBack });
-  }, []);
-
-  useEffect(() => {
-    const filtered = EMBEDDERS.filter((embedder) =>
-      embedder.name.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-    setFilteredEmbedders(filtered);
-  }, [searchQuery, selectedEmbedder]);
+  const selectedEmbedderObject = EMBEDDERS.find(
+    (embedder) => embedder.value === selectedEmbedder
+  );
 
   return (
-    <div>
-      <form ref={formRef} onSubmit={handleSubmit} className="w-full">
-        <div className="w-full relative border-slate-300/40 shadow border-2 rounded-lg text-white">
-          <div className="w-full p-4 absolute top-0 rounded-t-lg backdrop-blur-sm">
-            <div className="w-full flex items-center sticky top-0 z-20">
-              <MagnifyingGlass
-                size={16}
-                weight="bold"
-                className="absolute left-4 z-30 text-white"
-              />
-              <input
-                type="text"
-                placeholder="Search Embedding providers"
-                className="bg-zinc-600 z-20 pl-10 h-[38px] rounded-full w-full px-4 py-1 text-sm border-2 border-slate-300/40 outline-none focus:border-white text-white"
-                onChange={(e) => setSearchQuery(e.target.value)}
-                autoComplete="off"
-                onKeyDown={(e) => {
-                  if (e.key === "Enter") e.preventDefault();
-                }}
-              />
+    <form
+      ref={formRef}
+      onSubmit={handleSubmit}
+      className="w-full flex flex-col items-center"
+    >
+      <div className="relative">
+        {searchMenuOpen ? (
+          <div className="w-full max-w-[753px] max-h-[310px] overflow-auto white-scrollbar min-h-[64px] bg-[#18181B] rounded-lg flex flex-col justify-between cursor-pointer border-2 border-[#46C8FF] z-20">
+            <div className="w-full flex flex-col gap-y-1">
+              <div className="flex items-center sticky top-0 border-b border-[#9CA3AF] mx-4 bg-[#18181B]">
+                <MagnifyingGlass
+                  size={20}
+                  weight="bold"
+                  className="absolute left-4 z-30 text-white -ml-4 my-2"
+                />
+                <input
+                  type="text"
+                  name="embedder-search"
+                  placeholder="Search all Embedding providers"
+                  className="-ml-4 my-2 bg-transparent z-20 pl-12 h-[38px] w-full px-4 py-1 text-sm outline-none focus:border-white text-white placeholder:text-white placeholder:font-medium"
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                  ref={searchInputRef}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") e.preventDefault();
+                  }}
+                />
+                <X
+                  size={20}
+                  weight="bold"
+                  className="cursor-pointer text-white hover:text-[#9CA3AF]"
+                  onClick={handleXButton}
+                />
+              </div>
+              <div className="flex-1 pl-4 pr-2 flex flex-col gap-y-1 overflow-y-auto white-scrollbar pb-4">
+                {filteredEmbedders.map((embedder) => (
+                  <EmbedderItem
+                    key={embedder.name}
+                    name={embedder.name}
+                    value={embedder.value}
+                    image={embedder.logo}
+                    description={embedder.description}
+                    checked={selectedEmbedder === embedder.value}
+                    onClick={() => updateEmbedderChoice(embedder.value)}
+                  />
+                ))}
+              </div>
             </div>
           </div>
-          <div className="px-4 pt-[70px] flex flex-col gap-y-1 max-h-[390px] overflow-y-auto no-scroll pb-4">
-            {filteredEmbedders.map((embedder) => {
-              return (
-                <EmbedderItem
-                  key={embedder.name}
-                  name={embedder.name}
-                  value={embedder.value}
-                  image={embedder.logo}
-                  description={embedder.description}
-                  checked={selectedEmbedder === embedder.value}
-                  onClick={() => setSelectedEmbedder(embedder.value)}
-                />
-              );
-            })}
-          </div>
+        ) : (
+          <button
+            className="w-full max-w-[753px] h-[64px] bg-[#18181B] rounded-lg flex items-center p-[14px] justify-between cursor-pointer border-2 border-transparent hover:border-[#46C8FF] transition-all duration-300"
+            type="button"
+            onClick={() => setSearchMenuOpen(true)}
+          >
+            <div className="flex gap-x-4 items-center">
+              <img
+                src={selectedEmbedderObject.logo}
+                alt={`${selectedEmbedderObject.name} logo`}
+                className="w-10 h-10 rounded-md"
+              />
+              <div className="flex flex-col text-left">
+                <div className="text-sm font-semibold text-white">
+                  {selectedEmbedderObject.name}
+                </div>
+                <div className="mt-1 text-xs text-[#D2D5DB]">
+                  {selectedEmbedderObject.description}
+                </div>
+              </div>
+            </div>
+            <CaretUpDown size={24} weight="bold" className="text-white" />
+          </button>
+        )}
+        <div className="mt-4 flex flex-col gap-y-1 justify-start w-full">
+          {selectedEmbedderObject && selectedEmbedderObject.options}
         </div>
-        <div className="mt-4 flex flex-col gap-y-1">
-          {selectedEmbedder &&
-            EMBEDDERS.find((embedder) => embedder.value === selectedEmbedder)
-              ?.options}
-        </div>
-        <button
-          type="submit"
-          ref={hiddenSubmitButtonRef}
-          hidden
-          aria-hidden="true"
-        ></button>
-      </form>
-    </div>
+      </div>
+
+      <button
+        type="submit"
+        ref={hiddenSubmitButtonRef}
+        hidden
+        aria-hidden="true"
+      ></button>
+    </form>
   );
 }
