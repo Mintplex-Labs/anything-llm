bb790e87ad3ac9ee40ad2b58a94e64004f9e08d5
timothycarambat <rambat1010@gmail.com>
2024-04-24 17:42:43 -0700

    do not drop-replace users table in migration

--- server/prisma/migrations/20240425004220_init/migration.sql
+++ server/prisma/migrations/20240425004220_init/migration.sql
@@ -1,3 +1,6 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN "seen_recovery_codes" BOOLEAN DEFAULT false;
+
 -- CreateTable
 CREATE TABLE "recovery_codes" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -16,26 +19,6 @@ CREATE TABLE "password_reset_tokens" (
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT "password_reset_tokens_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
-
--- RedefineTables
-PRAGMA foreign_keys=OFF;
-CREATE TABLE "new_users" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "username" TEXT,
-    "password" TEXT NOT NULL,
-    "pfpFilename" TEXT,
-    "role" TEXT NOT NULL DEFAULT 'default',
-    "suspended" INTEGER NOT NULL DEFAULT 0,
-    "seen_recovery_codes" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "lastUpdatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-INSERT INTO "new_users" ("createdAt", "id", "lastUpdatedAt", "password", "pfpFilename", "role", "suspended", "username") SELECT "createdAt", "id", "lastUpdatedAt", "password", "pfpFilename", "role", "suspended", "username" FROM "users";
-DROP TABLE "users";
-ALTER TABLE "new_users" RENAME TO "users";
-CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
-PRAGMA foreign_key_check;
-PRAGMA foreign_keys=ON;
 
 -- CreateIndex
 CREATE INDEX "recovery_codes_user_id_idx" ON "recovery_codes"("user_id");
