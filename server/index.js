2323f7616297e544ade8563725e02f132636190a
timothycarambat <rambat1010@gmail.com>
2024-04-26 15:31:07 -0700

    Prevent iframing of frontend UI to prevent unsafe embedding and/or clickjacking

--- server/index.js
+++ server/index.js
@@ -56,7 +56,14 @@ embeddedEndpoints(apiRouter);
 
 if (process.env.NODE_ENV !== "development") {
   app.use(
-    express.static(path.resolve(__dirname, "public"), { extensions: ["js"] })
+    express.static(path.resolve(__dirname, "public"), {
+      extensions: ["js"],
+      setHeaders: (res) => {
+        // Disable I-framing of entire site UI
+        res.removeHeader("X-Powered-By");
+        res.setHeader("X-Frame-Options", "DENY");
+      },
+    })
   );
 
   app.use("/", function (_, response) {
