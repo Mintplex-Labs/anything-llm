dc21c3f1a54ed0822d5d9af704a84fed5d62318e
hdavy2002 <hdavy2002@gmail.com>
2024-05-07 11:39:32 +0530

    br

--- frontend/src/components/DefaultChat/index.jsx
+++ frontend/src/components/DefaultChat/index.jsx
@@ -53,14 +53,14 @@ export default function DefaultChatContainer() {
             >
               Hey there! I'm your AI companion, here to lend a hand and assist you however I can. Just a heads up, 
               I might not always get things perfect, so I apologize in advance for any slip-ups. If I give you any wrong info,
-              feel free to reach out to my creator with your feedback.<br><br>
+              feel free to reach out to my creator with your feedback.<br /><br />
               To get the most out of our chats, start conversations with the Hubs in the sidebar. Each Hub has its own 
-              specialized training data, which helps me be more knowledgeable about specific topics.<br><br>
+              specialized training data, which helps me be more knowledgeable about specific topics.<br /><br />
               So, who am I exactly? Well, I'm your friendly AI chat bot, and you can think of me as having a home in 
               data centers. My main spot is in Germany, but my creator might move me around to places like the US, Canada, 
               Australia, and beyond. Don't worry, though—our chats are totally private, and nothing leaves this server. 
               Unlike some other big players out there who use your data to train themselves, I never do that. Once this 
-              server is gone, any memory of our conversations is history. <br><br>
+              server is gone, any memory of our conversations is history. <br /><br />
               I rely on NVIDIA GPUs to power my brain, so if I'm not lightning-fast, just hang in there with me. Even 
               AI needs a bit of time to mull things over, right?
             </span>
