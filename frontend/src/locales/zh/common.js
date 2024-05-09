47875e731d8186035ed6adac1b5e42fe3db66374
GetOffer.help <13744916+getofferhelp@users.noreply.github.com>
2024-05-09 23:21:34 +0800

    Update common.js (#1320)

--- frontend/src/locales/zh/common.js
+++ frontend/src/locales/zh/common.js
@@ -3,17 +3,17 @@ const TRANSLATIONS = {
   // Setting Sidebar menu items.
   settings: {
     title: "设置",
-    system: null,
-    invites: null,
-    users: null,
-    workspaces: null,
-    "workspace-chats": "对话工作区",
+    system: "系统",
+    invites: "邀请",
+    users: "用户",
+    workspaces: "工作区",
+    "workspace-chats": "对话历史记录", //  "workspace-chats" should be  "对话历史记录", means "chat history",or "chat history records"
     appearance: "外观",
     "api-keys": "API 密钥",
     llm: "LLM 首选项",
-    transcription: "Transcription 模型",
+    transcription: "Transcription 模型", 
     embedder: "Embedder 首选项",
-    "text-splitting": null,
+    "text-splitting": "文本分割",
     "vector-database": "向量数据库",
     embeds: "嵌入式对话",
     "embed-chats": "嵌入式对话历史",
@@ -24,11 +24,11 @@ const TRANSLATIONS = {
   // Page Definitions
   login: {
     "multi-user": {
-      welcome: null,
-      "forgot-pass": null,
-      reset: null,
+      welcome: "欢迎！",
+      "forgot-pass": "忘记密码",
+      reset: "重置",
     },
-    "sign-in": null,
+    "sign-in": "登录",
   },
 };
 
