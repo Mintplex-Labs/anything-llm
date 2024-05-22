e208074ef4c240fe03e4147ab097ec3b52b97619
timothycarambat <rambat1010@gmail.com>
2024-05-22 11:50:01 -0500

    patch path normalization

--- collector/utils/files/index.js
+++ collector/utils/files/index.js
@@ -122,7 +122,7 @@ function isWithin(outer, inner) {
 
 function normalizePath(filepath = "") {
   const result = path
-    .normalize(filepath.trim())
+    .normalize(filepath.replace(/\s/g, "-").trim())
     .replace(/^(\.\.(\/|\\|$))+/, "")
     .trim();
   if (["..", ".", "/"].includes(result)) throw new Error("Invalid path.");
