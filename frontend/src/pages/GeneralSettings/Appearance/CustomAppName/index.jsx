11506ab1ae59b88259f36124f8a62e7d89ae1841
hdavy2002 <hdavy2002@gmail.com>
2024-06-01 03:31:07 +0530

    custom

--- frontend/src/pages/GeneralSettings/Appearance/CustomAppName/index.jsx
+++ frontend/src/pages/GeneralSettings/Appearance/CustomAppName/index.jsx
@@ -60,7 +60,7 @@ export default function CustomAppName() {
     <form className="mb-6" onSubmit={updateCustomAppName}>
       <div className="flex flex-col gap-y-1">
         <h2 className="text-base leading-6 font-bold text-white">
-          Custom App Name
+          Custom ChatLTT App Name
         </h2>
         <p className="text-xs leading-[18px] font-base text-white/60">
           Set a custom app name that is displayed on the login page.
