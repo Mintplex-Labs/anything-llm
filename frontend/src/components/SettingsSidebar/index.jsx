f1c6f1352d502e215af5824cccf218443c2b0ab7
hdavy2002 <hdavy2002@gmail.com>
2024-06-03 13:31:04 +0530

    Deleted Hub Chat

--- frontend/src/components/SettingsSidebar/index.jsx
+++ frontend/src/components/SettingsSidebar/index.jsx
@@ -255,7 +255,7 @@ const SidebarOptions = ({ user = null }) => (
       icon={<ChatCenteredText className="h-5 w-5 flex-shrink-0" />}
       user={user}
       flex={true}
-      allowedRole={["admin", "manager"]}
+      allowedRole={["admin"]}
     />
     <Option
       href={paths.settings.appearance()}
