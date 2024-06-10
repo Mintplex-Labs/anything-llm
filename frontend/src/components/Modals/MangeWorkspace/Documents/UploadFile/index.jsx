ae22f2a32656a8d82d5cbe18ceb060e6ba14fa14
hdavy2002 <hdavy2002@gmail.com>
2024-06-10 16:17:27 +0530

    ins

--- frontend/src/components/Modals/MangeWorkspace/Documents/UploadFile/index.jsx
+++ frontend/src/components/Modals/MangeWorkspace/Documents/UploadFile/index.jsx
@@ -164,7 +164,7 @@ export default function UploadFile({
       </form>
       <div className="mt-6 text-center text-white text-opacity-80 text-xs font-medium w-[560px]">
         These files will be uploaded to the document processor running on this
-        ChatLTT instance. These files are not sent or shared with a third
+        instance. These files are not sent or shared with a third
         party.
       </div>
     </div>
