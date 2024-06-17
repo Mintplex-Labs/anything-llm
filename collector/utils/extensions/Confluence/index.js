8ef511c7ba423e25c70746dccf22c3e5939eeeb8
timothycarambat <rambat1010@gmail.com>
2024-06-17 16:02:59 -0700

    refactor implementation of various types of Confluence URL patterns

--- collector/utils/extensions/Confluence/index.js
+++ collector/utils/extensions/Confluence/index.js
@@ -9,40 +9,6 @@ const {
   ConfluencePagesLoader,
 } = require("langchain/document_loaders/web/confluence");
 
-function urlMatchesPatter(url, patter) {
-  const urlPattern = new UrlPattern(patter);
-  return urlPattern.match(url);
-}
-
-function generateCustomDomain({ subdomain, domain, tld }) {
-  return (subdomain ? `${subdomain}.` : "") + `${domain}.${tld}`;
-}
-
-function validSpaceUrl(spaceUrl = "") {
-  // Atlassian default URL match
-  const atlassianMatch = urlMatchesPatter(spaceUrl, "https\\://(:subdomain).atlassian.net/wiki/spaces/(:spaceKey)*");
-  if (atlassianMatch) {
-    return { valid: true, result: atlassianMatch };
-  }
-
-  // Custom URL match
-  const customMatch = urlMatchesPatter(spaceUrl, "https\\://(:subdomain.):domain.:tld/wiki/spaces/(:spaceKey)*");
-  if (customMatch) {
-    customMatch.customDomain = generateCustomDomain(customMatch);
-    return { valid: true, result: customMatch, humanReadable: false };
-  }
-
-  // Human Readable URL match
-  const humanReadableMatch = urlMatchesPatter(spaceUrl, "https\\://(:subdomain.):domain.:tld/display/(:spaceKey)*");
-  if (humanReadableMatch) {
-    humanReadableMatch.customDomain = generateCustomDomain(humanReadableMatch);
-    return { valid: true, result: humanReadableMatch, humanReadable: true };
-  }
-
-  // No match
-  return { valid: false, result: null };
-}
-
 async function loadConfluence({ pageUrl, username, accessToken }) {
   if (!pageUrl || !username || !accessToken) {
     return {
@@ -52,9 +18,8 @@ async function loadConfluence({ pageUrl, username, accessToken }) {
     };
   }
 
-  const validSpace = validSpaceUrl(pageUrl);
-  const { result: validSpaceResult, humanReadable } = validSpace;
-  if (!validSpaceResult) {
+  const { valid, result } = validSpaceUrl(pageUrl);
+  if (!valid) {
     return {
       success: false,
       reason:
@@ -62,13 +27,7 @@ async function loadConfluence({ pageUrl, username, accessToken }) {
     };
   }
 
-  const { subdomain, customDomain, spaceKey } = validSpaceResult;
-  let subpath = humanReadable ? `` : `/wiki`;
-  let baseUrl = `https://${subdomain}.atlassian.net${subpath}`;
-  if (customDomain) {
-    baseUrl = `https://${customDomain}${subpath}`;
-  }
-
+  const { apiBase: baseUrl, spaceKey, subdomain } = result;
   console.log(`-- Working Confluence ${baseUrl} --`);
   const loader = new ConfluencePagesLoader({
     baseUrl,
@@ -145,6 +104,95 @@ async function loadConfluence({ pageUrl, username, accessToken }) {
       destination: outFolder,
     },
   };
+}
+
+/**
+ * A match result for a url-pattern of a Confluence URL
+ * @typedef {Object} ConfluenceMatchResult
+ * @property {string} subdomain - the subdomain of an organization's Confluence space
+ * @property {string} spaceKey - the spaceKey of an organization that determines the documents to collect.
+ * @property {string} apiBase - the correct REST API url to use for loader.
+ */
+
+/**
+ * Generates the correct API base URL for interfacing with the Confluence REST API
+ * depending on the URL pattern being used since there are various ways to host/access a
+ * Confluence space.
+ * @param {ConfluenceMatchResult} matchResult - result from `url-pattern`.match
+ * @param {boolean} isCustomDomain - determines if we need to coerce the subpath of the provided URL
+ * @returns {string} - the resulting REST API URL
+ */
+function generateAPIBaseUrl(matchResult = {}, isCustomDomain = false) {
+  const { subdomain } = matchResult;
+  let subpath = isCustomDomain ? `` : `/wiki`;
+  if (isCustomDomain) return `https://${customDomain}${subpath}`;
+  return `https://${subdomain}.atlassian.net${subpath}`;
+}
+
+/**
+ * Validates and parses the correct information from a given Confluence URL
+ * @param {string} spaceUrl - The organization's Confluence URL to parse
+ * @returns {{
+ *  valid: boolean,
+ *  result: (ConfluenceMatchResult|null),
+ * }}
+ */
+function validSpaceUrl(spaceUrl = "") {
+  let matchResult;
+  const patterns = {
+    default: new UrlPattern(
+      "https\\://(:subdomain).atlassian.net/wiki/spaces/(:spaceKey)*"
+    ),
+    subdomain: new UrlPattern(
+      "https\\://(:subdomain.):domain.:tld/wiki/spaces/(:spaceKey)*"
+    ),
+    custom: new UrlPattern(
+      "https\\://(:subdomain.):domain.:tld/display/(:spaceKey)*"
+    ),
+  };
+
+  // If using the default Atlassian Confluence URL pattern.
+  // We can proceed because the Library/API can use this base url scheme.
+  matchResult = patterns.default.match(spaceUrl);
+  if (matchResult)
+    return {
+      valid: matchResult.hasOwnProperty("spaceKey"),
+      result: {
+        ...matchResult,
+        apiBase: generateAPIBaseUrl(matchResult),
+      },
+    };
+
+  // If using a custom subdomain Confluence URL pattern.
+  // We need to attach the customDomain as a property to the match result
+  // so we can form the correct REST API base from the subdomain.
+  matchResult = patterns.subdomain.match(spaceUrl);
+  if (matchResult) {
+    return {
+      valid: matchResult.hasOwnProperty("spaceKey"),
+      result: {
+        ...matchResult,
+        apiBase: generateAPIBaseUrl(matchResult),
+      },
+    };
+  }
+
+  // If using a base FQDN Confluence URL pattern.
+  // We need to attach the customDomain as a property to the match result
+  // so we can form the correct REST API base from the root domain since /display/ is basically a URL mask.
+  matchResult = patterns.custom.match(spaceUrl);
+  if (matchResult) {
+    return {
+      valid: matchResult.hasOwnProperty("spaceKey"),
+      result: {
+        ...matchResult,
+        apiBase: generateAPIBaseUrl(matchResult, true),
+      },
+    };
+  }
+
+  // No match
+  return { valid: false, result: null };
 }
 
 module.exports = loadConfluence;
