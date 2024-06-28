01c469ff67c3aaf48538ba35c5b53c445281475d
timothycarambat <rambat1010@gmail.com>
2024-06-28 15:30:36 -0700

    Deprecate `.isSafe` moderation Add type defs to helpers

--- server/utils/AiProviders/koboldCPP/index.js
+++ server/utils/AiProviders/koboldCPP/index.js
@@ -79,11 +79,6 @@ class KoboldCPPLLM {
     return [prompt, ...chatHistory, { role: "user", content: userPrompt }];
   }
 
-  async isSafe(_input = "") {
-    // Not implemented so must be stubbed
-    return { safe: true, reasons: [] };
-  }
-
   async getChatCompletion(messages = null, { temperature = 0.7 }) {
     const result = await this.openai.chat.completions
       .create({
