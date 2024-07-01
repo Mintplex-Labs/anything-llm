b21034053fa51ee107c201c1648cc0ba0e567de7
timothycarambat <rambat1010@gmail.com>
2024-07-01 16:25:00 -0700

    Patch Embed styles with prefixing

--- embed/src/main.jsx
+++ embed/src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
+import { parseStylesSrc } from "./utils/constants.js";
 const appElement = document.createElement("div");
 
 document.body.appendChild(appElement);
@@ -17,6 +19,13 @@ const scriptSettings = Object.assign(
 );
 export const embedderSettings = {
   settings: scriptSettings,
-  USER_STYLES: `bg-[${scriptSettings?.userBgColor ?? "#3DBEF5"}] text-white rounded-t-[18px] rounded-bl-[18px] rounded-br-[4px] mx-[20px]`,
-  ASSISTANT_STYLES: `bg-[${scriptSettings?.assistantBgColor ?? "#FFFFFF"}] text-[#222628] rounded-t-[18px] rounded-br-[18px] rounded-bl-[4px] mr-[37px] ml-[9px]`,
+  stylesSrc: parseStylesSrc(document?.currentScript?.src),
+  USER_STYLES: {
+    msgBg: scriptSettings?.userBgColor ?? "#3DBEF5",
+    base: `allm-text-white allm-rounded-t-[18px] allm-rounded-bl-[18px] allm-rounded-br-[4px] allm-mx-[20px]`,
+  },
+  ASSISTANT_STYLES: {
+    msgBg: scriptSettings?.userBgColor ?? "#FFFFFF",
+    base: `allm-text-[#222628] allm-rounded-t-[18px] allm-rounded-br-[18px] allm-rounded-bl-[4px] allm-mr-[37px] allm-ml-[9px]`,
+  },
 };
