074af447c908cd15b73fe5841b3da34de424e335
kashifmian722 <64021544+kashifmian722@users.noreply.github.com>
2024-07-06 13:50:43 +0500

    Update index.jsx

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -49,28 +49,33 @@ export default function Footer() {
     return (
       <div className="flex justify-center mb-2">
         <div className="flex space-x-4">
-         {/* <ToolTipWrapper id="open-github">
+          <ToolTipWrapper id="open-github">
             <a
               href={paths.github()}
               target="_blank"
-	@@ -61,7 +61,7 @@ export default function Footer() {
+              rel="noreferrer"
+              className="transition-all duration-300 p-2 rounded-full text-white bg-sidebar-button hover:bg-menu-item-selected-gradient hover:border-slate-100 hover:border-opacity-50 border-transparent border"
+              aria-label="Find us on Github"
+              data-tooltip-id="open-github"
+              data-tooltip-content="View source code on Github"
             >
               <GithubLogo weight="fill" className="h-5 w-5 " />
             </a>
-          </ToolTipWrapper> */}
+          </ToolTipWrapper>
           <ToolTipWrapper id="open-documentation">
             <a
               href={paths.docs()}
-	@@ -70,28 +70,28 @@ export default function Footer() {
+              target="_blank"
+              rel="noreferrer"
               className="w-fit transition-all duration-300 p-2 rounded-full text-white bg-sidebar-button hover:bg-menu-item-selected-gradient hover:border-slate-100 hover:border-opacity-50 border-transparent border"
               aria-label="Docs"
               data-tooltip-id="open-documentation"
-              data-tooltip-content="Open Fabriiq help docs"
+              data-tooltip-content="Open AnythingLLM help docs"
             >
               <BookOpen weight="fill" className="h-5 w-5 " />
             </a>
           </ToolTipWrapper>
-        {/*  <ToolTipWrapper id="open-discord">
+          <ToolTipWrapper id="open-discord">
             <a
               href={paths.discord()}
               target="_blank"
@@ -78,15 +83,56 @@ export default function Footer() {
               className="transition-all duration-300 p-2 rounded-full text-white bg-sidebar-button hover:bg-menu-item-selected-gradient hover:border-slate-100 hover:border-opacity-50 border-transparent border"
               aria-label="Join our Discord server"
               data-tooltip-id="open-discord"
-              data-tooltip-content="Join the Fabriiq Discord"
+              data-tooltip-content="Join the AnythingLLM Discord"
             >
               <DiscordLogo
                 weight="fill"
                 className="h-5 w-5 stroke-slate-200 group-hover:stroke-slate-200"
               />
             </a>
-          </ToolTipWrapper>  */}
-          {!isMobile && <SettingsButton />} 
+          </ToolTipWrapper>
+          {!isMobile && <SettingsButton />}
         </div>
       </div>
     );
+  }
+
+  return (
+    <div className="flex justify-center mb-2">
+      <div className="flex space-x-4">
+        {footerData.map((item, index) => (
+          <a
+            key={index}
+            href={item.url}
+            target="_blank"
+            rel="noreferrer"
+            className="transition-all duration-300 p-2 rounded-full text-white bg-sidebar-button hover:bg-menu-item-selected-gradient hover:border-slate-100 hover:border-opacity-50 border-transparent border"
+          >
+            {React.createElement(
+              ICON_COMPONENTS?.[item.icon] ?? ICON_COMPONENTS.Info,
+              {
+                weight: "fill",
+                className: "h-5 w-5",
+              }
+            )}
+          </a>
+        ))}
+        {!isMobile && <SettingsButton />}
+      </div>
+    </div>
+  );
+}
+
+export function ToolTipWrapper({ id = v4(), children }) {
+  return (
+    <div className="flex w-fit">
+      {children}
+      <Tooltip
+        id={id}
+        place="top"
+        delayShow={300}
+        className="tooltip !text-xs z-99"
+      />
+    </div>
+  );
+}
