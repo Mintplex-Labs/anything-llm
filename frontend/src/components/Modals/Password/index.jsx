e7a2083e3c0a6d2d12275a6ed2814020c9a51e3e
kashifmian722 <64021544+kashifmian722@users.noreply.github.com>
2024-07-06 12:10:57 +0500

    hide logo image in sign in

--- frontend/src/components/Modals/Password/index.jsx
+++ frontend/src/components/Modals/Password/index.jsx
@@ -34,14 +34,14 @@ export default function PasswordModal({ mode = "single" }) {
         />
       </div>
       <div className="flex flex-col items-center justify-center h-full w-full md:w-1/2 z-50 relative -mt-20">
-        <img
-          src={loginLogo}
-          alt="Logo"
-          className={`hidden relative md:flex rounded-2xl w-fit m-4 z-30 ${
-            mode === "single" ? "md:top-2" : "md:top-12"
-          } absolute max-h-[65px] md:bg-login-gradient md:shadow-[0_4px_14px_rgba(0,0,0,0.25)]`}
-          style={{ objectFit: "contain" }}
-        />
+              {/* <img
+  src={loginLogo}
+  alt="Logo"
+  className={`hidden relative md:flex rounded-2xl w-fit m-4 z-30 ${
+    mode === "single" ? "md:top-2" : "md:top-12"
+  } absolute max-h-[65px] md:bg-login-gradient md:shadow-[0_4px_14px_rgba(0,0,0,0.25)]`}
+  style={{ objectFit: "contain" }}
+/> */}
         {mode === "single" ? <SingleUserAuth /> : <MultiUserAuth />}
       </div>
     </div>
