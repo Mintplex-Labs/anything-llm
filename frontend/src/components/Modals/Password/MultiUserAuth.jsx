8aa265eab745b15352537fd78e8e374bc2fed210
kashifmian722 <64021544+kashifmian722@users.noreply.github.com>
2024-07-06 12:08:26 +0500

    frontend/src/components/Modals/Password/MultiUserAuth.jsx updated custom app name

--- frontend/src/components/Modals/Password/MultiUserAuth.jsx
+++ frontend/src/components/Modals/Password/MultiUserAuth.jsx
@@ -284,11 +284,11 @@ export default function MultiUserAuth() {
                   {t("login.multi-user.welcome")}
                 </h3>
                 <p className="text-4xl md:text-2xl font-bold bg-gradient-to-r from-[#75D6FF] via-[#FFFFFF] to-[#FFFFFF] bg-clip-text text-transparent">
-                  {customAppName || "AnythingLLM"}
+                  {customAppName || "Fabriiq"}
                 </p>
               </div>
               <p className="text-sm text-white/90 text-center">
-                {t("login.sign-in.start")} {customAppName || "AnythingLLM"}{" "}
+                {t("login.sign-in.start")} {customAppName || "Fabriiq"}{" "}
                 {t("login.sign-in.end")}
               </p>
             </div>
