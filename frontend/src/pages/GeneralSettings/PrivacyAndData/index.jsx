da843d5544ebcc0f8987524e756345d70798dad7
kashifmian722 <64021544+kashifmian722@users.noreply.github.com>
2024-07-06 12:35:24 +0500

    Update index.jsx

--- frontend/src/pages/GeneralSettings/PrivacyAndData/index.jsx
+++ frontend/src/pages/GeneralSettings/PrivacyAndData/index.jsx
@@ -184,11 +184,10 @@ function TelemetryLogs({ settings }) {
             based information. To see the list of event tags collected you can
             look on{" "}
             <a
-              href="https://github.com/search?q=repo%3AMintplex-Labs%2Fanything-llm%20.sendTelemetry(&type=code"
-              className="underline text-blue-400"
+              href="https://fabriiq.com/privacy"
               target="_blank"
             >
-              Github here
+              Privacy Docs
             </a>
             .
           </p>
@@ -197,13 +196,13 @@ function TelemetryLogs({ settings }) {
             dedicated to building the best solution for integrating AI and
             documents privately and securely. If you do decide to turn off
             telemetry all we ask is to consider sending us feedback and thoughts
-            so that we can continue to improve AnythingLLM for you.{" "}
+            so that we can continue to improve Fabriiq for you.{" "}
             <a
               href="mailto:team@mintplexlabs.com"
               className="underline text-blue-400"
               target="_blank"
             >
-              team@mintplexlabs.com
+              team@fabriiq.com
             </a>
             .
           </p>
