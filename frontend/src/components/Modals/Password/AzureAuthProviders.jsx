c7aa137ee92793f1a3a67fa3de72fb6551650f55
Bahtiar Ariyanki <bahtiarariyanki@Bahtiars-MacBook-Air.local>
2024-08-13 16:23:42 +0700

    224-incorporate-sso-into-anythingllm

--- frontend/src/components/Modals/Password/AzureAuthProviders.jsx
+++ frontend/src/components/Modals/Password/AzureAuthProviders.jsx
@@ -5,30 +5,14 @@ export default function AzureAuthProviders() {
   const { instance, accounts } = useMsal();
 
   useEffect(() => {
-    const handleRedirect = async () => {
-      try {
-        const response = await instance.handleRedirectPromise();
-        if (response) {
-          // Successfully handled redirect
-          console.log('User Account:', response.account);
-        }
-      } catch (error) {
-        console.error('Error handling redirect:', error);
-        // Handle error (e.g., show a message to the user)
-      }
-    };
-
-    if (accounts.length === 0) {
-      handleRedirect();
-    } else {
+    if (accounts.length > 0) {
       const account = accounts[0];
-
+      console.log('User Account:', account);
       instance.acquireTokenSilent({
         scopes: ["user.read"],
         account: account
       }).then(response => {
         console.log('Access Token:', response.accessToken);
-        // You can also use the accessToken to call APIs or fetch user data
       }).catch(error => {
         console.error('Token acquisition failed', error);
       });
