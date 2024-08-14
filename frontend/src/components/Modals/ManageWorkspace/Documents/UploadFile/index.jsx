3e0e9503e013aafa46127db49b5a24e6b0c93e82
Sukomal07 <hypersukomal@gmail.com>
2024-08-14 15:00:40 +0530

    added spanish in upload modal

--- frontend/src/components/Modals/ManageWorkspace/Documents/UploadFile/index.jsx
+++ frontend/src/components/Modals/ManageWorkspace/Documents/UploadFile/index.jsx
@@ -7,6 +7,7 @@ import System from "../../../../../models/system";
 import Workspace from "../../../../../models/workspace";
 import showToast from "../../../../../utils/toast";
 import FileUploadProgress from "./FileUploadProgress";
+import { useTranslation } from "react-i18next";
 
 export default function UploadFile({
   workspace,
@@ -18,6 +19,7 @@ export default function UploadFile({
   const [ready, setReady] = useState(false);
   const [files, setFiles] = useState([]);
   const [fetchingUrl, setFetchingUrl] = useState(false);
+  const { t } = useTranslation();
 
   const handleSendLink = async (e) => {
     e.preventDefault();
@@ -82,9 +84,8 @@ export default function UploadFile({
       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
       <div
-        className={`w-[280px] border-2 border-dashed rounded-2xl bg-zinc-900/50 p-3 ${
-          ready ? "cursor-pointer" : "cursor-not-allowed"
-        } hover:bg-zinc-900/90`}
+        className={`w-[280px] border-2 border-dashed rounded-2xl bg-zinc-900/50 p-3 ${ready ? "cursor-pointer" : "cursor-not-allowed"
+          } hover:bg-zinc-900/90`}
         {...getRootProps()}
         style={{ minWidth: !isUploadedDoc ? "41.3rem" : "3rem" }}
       >
@@ -93,21 +94,20 @@ export default function UploadFile({
           <div className="flex flex-col items-center justify-center h-full">
             <CloudArrowUp className="w-8 h-8 text-white/80" />
             <div className="text-white text-opacity-80 text-sm font-semibold py-1">
-              Document Processor Unavailable
+              {t("modal.error.title")}
             </div>
             <div className="text-white text-opacity-60 text-xs font-medium py-1 px-20 text-center">
-              We can't upload your files right now because the document
-              processor is offline. Please try again later.
+              {t("modal.error.description")}
             </div>
           </div>
         ) : files.length === 0 ? (
           <div className="flex flex-col items-center justify-center">
             <CloudArrowUp className="w-8 h-8 text-white/80" />
             <div className="text-white text-opacity-80 text-sm font-semibold py-1">
-              Click to upload or drag and drop
+              {t("modal.upload.title")}
             </div>
             <div className="text-white text-opacity-60 text-xs font-medium py-1">
-              supports text files, csv's, spreadsheets, audio files, and more!
+              {t("modal.upload.description")}
             </div>
           </div>
         ) : (
@@ -158,9 +158,7 @@ export default function UploadFile({
         </button>
       </form>
       <div className="mt-6 text-center text-white text-opacity-80 text-xs font-medium w-[280px]">
-        These files will be uploaded to the document processor running on this
-        AnythingLLM instance. These files are not sent or shared with a third
-        party.
+        {t("modal.info")}
       </div>
     </div>
   );
