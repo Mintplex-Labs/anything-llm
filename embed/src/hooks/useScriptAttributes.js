ea35e6a13937c0b58160b9d56fb2be6b20f498ad
timothycarambat <rambat1010@gmail.com>
2024-08-16 18:53:25 -0700

    Support suggestions of messages for empty chat in widget

--- embed/src/hooks/useScriptAttributes.js
+++ embed/src/hooks/useScriptAttributes.js
@@ -31,6 +31,7 @@ const DEFAULT_SETTINGS = {
   openOnLoad: "off", // or "on"
   supportEmail: null, // string of email for contact
   username: null, // The display or readable name set on a script
+  defaultMessages: [], // list of strings for default messages.
 };
 
 export default function useGetScriptAttributes() {
@@ -52,7 +53,7 @@ export default function useGetScriptAttributes() {
 
       setSettings({
         ...DEFAULT_SETTINGS,
-        ...embedderSettings.settings,
+        ...parseAndValidateEmbedSettings(embedderSettings.settings),
         loaded: true,
       });
     }
@@ -60,4 +61,44 @@ export default function useGetScriptAttributes() {
   }, [document]);
 
   return settings;
+}
+
+const validations = {
+  _fallbacks: {
+    defaultMessages: [],
+  },
+
+  defaultMessages: function (value = null) {
+    if (typeof value !== "string") return this._fallbacks.defaultMessages;
+    try {
+      const list = value.split(",");
+      if (
+        !Array.isArray(list) ||
+        list.length === 0 ||
+        !list.every((v) => typeof v === "string" && v.length > 0)
+      )
+        throw new Error(
+          "Invalid default-messages attribute value. Must be array of strings"
+        );
+      return list.map((v) => v.trim());
+    } catch (e) {
+      console.error("AnythingLLMEmbed", e);
+      return this._fallbacks.defaultMessages;
+    }
+  },
+};
+
+function parseAndValidateEmbedSettings(settings = {}) {
+  const validated = {};
+  for (let [key, value] of Object.entries(settings)) {
+    if (!validations.hasOwnProperty(key)) {
+      validated[key] = value;
+      continue;
+    }
+
+    const validatedValue = validations[key](value);
+    validated[key] = validatedValue;
+  }
+
+  return validated;
 }
