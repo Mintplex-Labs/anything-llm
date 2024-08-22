3a3399af94e0b0c83812deb91ef48fb6a7f49f04
Timothy Carambat <rambat1010@gmail.com>
2024-08-22 15:31:36 -0700

    Move `embed` to submodule (#2163)
    * Move `embed` to submodule
    
    * update README

--- embed/src/utils/date.js
+++
@@ -1,9 +0,0 @@
-export function formatDate(sentAt) {
-  const date = new Date(sentAt * 1000);
-  const timeString = date.toLocaleTimeString([], {
-    hour: "numeric",
-    minute: "2-digit",
-    hour12: true,
-  });
-  return timeString;
-}
