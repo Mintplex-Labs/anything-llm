57030e5d58df1f1ef32d3a6e17eb3b6cf84b08c7
K <likaiwei99@gmail.com>
2024-09-04 10:29:24 +0800

    Update github link

--- frontend/src/utils/paths.js
+++ frontend/src/utils/paths.js
@@ -34,7 +34,7 @@ export default {
     },
   },
   github: () => {
-    return "https://github.com/Mintplex-Labs/anything-llm";
+    return "https://github.com/likw99/anything-llm";
   },
   discord: () => {
     return "https://discord.com/invite/6UyHPeGZAC";
