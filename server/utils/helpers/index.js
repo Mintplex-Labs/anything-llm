12da1e8db9373ecfdbdb606e07fac271fc0db978
timothycarambat <rambat1010@gmail.com>
2024-09-16 12:09:28 -0700

    class only return

--- server/utils/helpers/index.js
+++ server/utils/helpers/index.js
@@ -245,7 +245,7 @@ function getLLMProviderClass({ provider = null } = {}) {
       return TogetherAiLLM;
     case "fireworksai":
       const { FireworksAiLLM } = require("../AiProviders/fireworksAi");
-      return new FireworksAiLLM();
+      return FireworksAiLLM;
     case "perplexity":
       const { PerplexityLLM } = require("../AiProviders/perplexity");
       return PerplexityLLM;
