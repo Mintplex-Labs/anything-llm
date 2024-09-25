05ac05ce083d15e8efe3ec482e7ee1c6e9f4e3f0
shubham bajaj <shmbajaj32@gmail.com>
2024-09-25 22:15:48 +0530

    feat: use memory buffer

--- collector/processSingleFile/convert/asImage.js
+++ collector/processSingleFile/convert/asImage.js
@@ -1,17 +1,9 @@
-const { v4 } = require("uuid");
-const fs = require("fs");
 const path = require("path");
-const {
-  writeToServerDocuments,
-  createdDate,
-  trashFile,
-} = require("../../utils/files");
 const { tokenizeString } = require("../../utils/tokenizer");
-const { default: slugify } = require("slugify");
 const { S3Service } = require("../../utils/s3");
 const { TextractService } = require("../../utils/textract");
 
-async function asImage({ fullFilePath = "", filename = "" }) {
+async function asImage({ fullFilePath = "", filename = "", uploadedFile }) {
   const BUCKET_NAME = process.env.S3_BUCKET_NAME;
   if (!BUCKET_NAME) {
     return {
@@ -21,24 +13,17 @@ async function asImage({ fullFilePath = "", filename = "" }) {
   }
   try {
     console.log(`-- Working ${filename} --`);
-    const uuid = v4();
-    const uniqueFilename = `${uuid}-${filename}`;
     const s3Service = new S3Service();
     const textractService = new TextractService();
-    const fileUploadUrl = await s3Service.uploadFileToS3(
-      fullFilePath,
-      BUCKET_NAME,
-      uuid
-    );
 
     const extractedText = await textractService.analyzeS3Document(
       BUCKET_NAME,
-      uniqueFilename
+      uploadedFile.filename
     );
     const fileNameWithoutExt = path.parse(filename).name;
     const pageContentParams = {
       Bucket: BUCKET_NAME,
-      Key: `pageContents/${uuid}-${fileNameWithoutExt}.txt`,
+      Key: `pageContents/${uploadedFile.uuid}-${fileNameWithoutExt}.txt`,
       Body: extractedText,
     };
 
@@ -49,23 +34,24 @@ async function asImage({ fullFilePath = "", filename = "" }) {
       pageContentParams
     );
 
+    const publishedDate = new Date(); // Use the current date and time
+
     const data = {
-      url: fileUploadUrl,
-      storageKey: uuid,
+      url: uploadedFile.url,
+      storageKey: uploadedFile.uuid,
+      title: uploadedFile.originalname,
       pageContentUploadUrl: pageContentUploadUrl,
-      fileUploadUrl: fileUploadUrl,
-      title: filename,
+      fileUploadUrl: uploadedFile.url,
       docAuthor: "Unknown",
       description: "Unknown",
       docSource: "img file uploaded by the user.",
       chunkSource: "",
-      published: createdDate(fullFilePath),
+      published: publishedDate,
       wordCount: extractedText.split(" ").length,
       pageContent: extractedText,
       token_count_estimate: tokenizeString(extractedText).length,
     };
 
-    trashFile(fullFilePath);
     return { success: true, reason: null, documents: [data] };
   } catch (error) {
     console.error("An error occurred while processing the document:", error);
