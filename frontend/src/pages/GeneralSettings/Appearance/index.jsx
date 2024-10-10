b2d16efcfb8f0aa06e0e15a029e67310102aa1c2
shatfield4 <seanhatfield5@gmail.com>
2024-10-10 16:49:53 -0700

    appearance settings styles

--- frontend/src/pages/GeneralSettings/Appearance/index.jsx
+++ frontend/src/pages/GeneralSettings/Appearance/index.jsx
@@ -9,7 +9,7 @@ import CustomAppName from "./CustomAppName";
 import LanguagePreference from "./LanguagePreference";
 import CustomSiteSettings from "./CustomSiteSettings";
 import ShowScrollbar from "./ShowScrollbar";
-import ThemePreference from "./ThemePreference";
+// import ThemePreference from "./ThemePreference";
 
 export default function Appearance() {
   const { t } = useTranslation();
@@ -19,7 +19,7 @@ export default function Appearance() {
       <Sidebar />
       <div
         style={{ height: isMobile ? "100%" : "calc(100% - 32px)" }}
-        className="relative md:ml-[2px] md:mr-[16px] md:my-[16px] md:rounded-[16px] bg-main-gradient w-full h-full overflow-y-scroll"
+        className="relative md:ml-[2px] md:mr-[16px] md:my-[16px] md:rounded-[16px] bg-theme-bg-secondary w-full h-full overflow-y-scroll p-4 md:p-0"
       >
         <div className="flex flex-col w-full px-1 md:pl-6 md:pr-[86px] md:py-6 py-16">
           <div className="w-full flex flex-col gap-y-1 pb-6 border-white border-b-2 border-opacity-10">
@@ -32,8 +32,8 @@ export default function Appearance() {
               {t("appearance.description")}
             </p>
           </div>
-
-          <ThemePreference />
+          {/* TODO: Enable when we have other themes */}
+          {/* <ThemePreference /> */}
           <LanguagePreference />
           <ShowScrollbar />
           <CustomLogo />
