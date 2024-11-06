5aa79128f7d935bf42fdd133c523337f041d81b0
timothycarambat <rambat1010@gmail.com>
2024-11-06 08:14:08 -0800

    bump Anthropic models

--- server/utils/AiProviders/anthropic/index.js
+++ server/utils/AiProviders/anthropic/index.js
@@ -46,9 +46,13 @@ class AnthropicLLM {
       "claude-instant-1.2",
       "claude-2.0",
       "claude-2.1",
-      "claude-3-opus-20240229",
-      "claude-3-sonnet-20240229",
       "claude-3-haiku-20240307",
+      "claude-3-sonnet-20240229",
+      "claude-3-opus-latest",
+      "claude-3-5-haiku-latest",
+      "claude-3-5-haiku-20241022",
+      "claude-3-5-sonnet-latest",
+      "claude-3-5-sonnet-20241022",
       "claude-3-5-sonnet-20240620",
     ];
     return validModels.includes(modelName);
