a82633af8c083ef9e561fa702065395776c9d65b
timothycarambat <rambat1010@gmail.com>
2024-12-30 16:40:54 -0800

    patch gemini

--- server/utils/AiProviders/gemini/index.js
+++ server/utils/AiProviders/gemini/index.js
@@ -168,6 +168,7 @@ class GeminiLLM {
     const models = await this.fetchModels(true);
     return models.some((model) => model.id === modelName);
   }
+
   /**
    * Generates appropriate content array for a message + attachments.
    * @param {{userPrompt:string, attachments: import("../../helpers").Attachment[]}}
@@ -220,8 +221,17 @@ class GeminiLLM {
       .map((message) => {
         if (message.role === "system")
           return { role: "user", parts: [{ text: message.content }] };
-        if (message.role === "user")
+
+        if (message.role === "user") {
+          // If the content is an array - then we have already formatted the context so return it directly.
+          if (Array.isArray(message.content))
+            return { role: "user", parts: message.content };
+
+          // Otherwise, this was a regular user message with no attachments
+          // so we need to format it for Gemini
           return { role: "user", parts: [{ text: message.content }] };
+        }
+
         if (message.role === "assistant")
           return { role: "model", parts: [{ text: message.content }] };
         return null;
