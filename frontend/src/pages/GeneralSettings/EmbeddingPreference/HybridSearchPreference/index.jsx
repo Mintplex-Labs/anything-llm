ddeb8967eaf1fc2e02e954a90a9e74db88c84499
salman-abdulqadir <salmanabdulkadir82@gmail.com>
2025-01-23 16:47:18 +0400

    DAD-67 | adjusting spaces

--- frontend/src/pages/GeneralSettings/EmbeddingPreference/HybridSearchPreference/index.jsx
+++ frontend/src/pages/GeneralSettings/EmbeddingPreference/HybridSearchPreference/index.jsx
@@ -8,8 +8,7 @@ const INPUT_CONFIGS = {
     inputProps: {
       type: "url",
       name: "SparseEmbeddingBasePath",
-      className:
-        "border-none bg-theme-settings-input-bg text-white placeholder:text-theme-settings-input-placeholder text-sm rounded-lg focus:outline-primary-button active:outline-primary-button outline-none block w-full p-2.5",
+
       placeholder: "https://api.openai.com/v1",
       required: true,
       autoComplete: "off",
@@ -21,8 +20,6 @@ const INPUT_CONFIGS = {
     inputProps: {
       type: "text",
       name: "SparseEmbeddingModelPref",
-      className:
-        "border-none bg-theme-settings-input-bg text-white placeholder:text-theme-settings-input-placeholder text-sm rounded-lg focus:outline-primary-button active:outline-primary-button outline-none block w-full p-2.5",
       placeholder: "text-embedding-ada-002",
       required: true,
       autoComplete: "off",
@@ -37,8 +34,6 @@ const INPUT_CONFIGS = {
       min: 0,
       step: 0.01,
       name: "HybridSearchDenseVectorWeight",
-      className:
-        "border-none bg-theme-settings-input-bg text-white placeholder:text-theme-settings-input-placeholder text-sm rounded-lg focus:outline-primary-button active:outline-primary-button outline-none block w-full p-2.5",
       placeholder: 0.8,
       required: true,
       autoComplete: "off",
@@ -53,8 +48,6 @@ const INPUT_CONFIGS = {
       min: 0,
       step: 0.01,
       name: "HybridSearchSparseVectorWeight",
-      className:
-        "border-none bg-theme-settings-input-bg text-white placeholder:text-theme-settings-input-placeholder text-sm rounded-lg focus:outline-primary-button active:outline-primary-button outline-none block w-full p-2.5",
       placeholder: 0.2,
       required: true,
       autoComplete: "off",
@@ -74,8 +67,6 @@ const INPUT_CONFIGS = {
       type: "password",
       name: "SparseGenericOpenAiEmbeddingApiKey",
       required: false,
-      className:
-        "border-none bg-theme-settings-input-bg text-white placeholder:text-theme-settings-input-placeholder text-sm rounded-lg focus:outline-primary-button active:outline-primary-button outline-none block w-full p-2.5",
       placeholder: "sk-mysecretkey",
       autoComplete: "off",
       spellCheck: false,
@@ -104,7 +95,11 @@ const HybridSearchPreference = ({ settings, onChange, weightError }) => {
             ) : (
               config.label
             )}
-            <input {...config?.inputProps} defaultValue={defaultValue} />
+            <input
+              {...config?.inputProps}
+              defaultValue={defaultValue}
+              className="border-none bg-theme-settings-input-bg text-white placeholder:text-theme-settings-input-placeholder text-sm rounded-lg focus:outline-primary-button active:outline-primary-button outline-none block w-full p-2.5"
+            />
           </div>
         );
       })}
@@ -115,52 +110,56 @@ const HybridSearchPreference = ({ settings, onChange, weightError }) => {
     useState(defaultChecked);
   return (
     <div className="flex flex-col gap-[24px]" onChange={onChange}>
-      <div className="flex items-center gap-2">
-        <div className="text-base font-bold text-white">
-          {t("embedding.hybrid-search.title")}
+      <div className="space-y-3">
+        <div className="flex items-center gap-2">
+          <div className="text-base font-bold text-white">
+            {t("embedding.hybrid-search.title")}
+          </div>
+          <label className="relative inline-flex cursor-pointer items-center">
+            <input
+              id="HybridSearchEnabled"
+              type="checkbox"
+              name="HybridSearchEnabled"
+              defaultChecked={defaultChecked}
+              onChange={() =>
+                setIsHybridSearchEnabled((previousState) => !previousState)
+              }
+              className="peer sr-only"
+            />
+            <div className="pointer-events-none peer h-6 w-11 rounded-full bg-[#CFCFD0] after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:shadow-xl after:border-none after:bg-white after:box-shadow-md after:transition-all after:content-[''] peer-checked:bg-[#32D583] peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-transparent"></div>
+          </label>
         </div>
-        <label className="relative inline-flex cursor-pointer items-center">
-          <input
-            id="HybridSearchEnabled"
-            type="checkbox"
-            name="HybridSearchEnabled"
-            defaultChecked={defaultChecked}
-            onChange={() =>
-              setIsHybridSearchEnabled((previousState) => !previousState)
-            }
-            className="peer sr-only"
-          />
-          <div className="pointer-events-none peer h-6 w-11 rounded-full bg-[#CFCFD0] after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:shadow-xl after:border-none after:bg-white after:box-shadow-md after:transition-all after:content-[''] peer-checked:bg-[#32D583] peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-transparent"></div>
-        </label>
-      </div>
-      <p className="text-xs leading-[18px] font-base text-white text-opacity-60">
-        {t("embedding.hybrid-search.desc")}
-      </p>
-      <div className="text-base font-bold text-white">
-        {t("embedding.provider.sparseTitle")}
+        <p className="text-xs leading-[18px] font-base text-white text-opacity-60">
+          {t("embedding.hybrid-search.desc")}
+        </p>
       </div>
       {isHybridSearchEnabled && (
-        <div className="flex flex-col gap-[24px]">
-          {renderInputFields([
-            INPUT_CONFIGS.baseUrl,
-            INPUT_CONFIGS.embeddingModel,
-            INPUT_CONFIGS.apiKey,
-          ])}
-          {renderInputFields([
-            INPUT_CONFIGS.denseEmbedderWeight,
-            INPUT_CONFIGS.sparseEmbedderWeight,
-          ])}
+        <div className="space-y-2">
+          <div className="text-base font-bold text-white">
+            {t("embedding.provider.sparseTitle")}
+          </div>
+          <div className="flex flex-col gap-[24px]">
+            {renderInputFields([
+              INPUT_CONFIGS.baseUrl,
+              INPUT_CONFIGS.embeddingModel,
+              INPUT_CONFIGS.apiKey,
+            ])}
+            {renderInputFields([
+              INPUT_CONFIGS.denseEmbedderWeight,
+              INPUT_CONFIGS.sparseEmbedderWeight,
+            ])}
 
-          <p
-            className={`flex items-center gap-2 text-xs leading-[18px] font-base ${weightError ? "text-danger" : "text-white text-opacity-60 "}`}
-          >
-            {weightError ? (
-              <WarningDiamond className="w-6 h-6" />
-            ) : (
-              <Info className="w-6 h-6" />
-            )}
-            {t("embedding.hybrid-search.weightInfo")}
-          </p>
+            <p
+              className={`flex items-center gap-2 text-xs leading-[18px] font-base ${weightError ? "text-danger" : "text-white text-opacity-60 "}`}
+            >
+              {weightError ? (
+                <WarningDiamond className="w-6 h-6" />
+              ) : (
+                <Info className="w-6 h-6" />
+              )}
+              {t("embedding.hybrid-search.weightInfo")}
+            </p>
+          </div>
         </div>
       )}
     </div>
