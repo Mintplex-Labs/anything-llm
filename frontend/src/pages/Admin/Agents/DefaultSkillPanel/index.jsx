8429d923afc8145ca2fe164030f81b72741ab5c1
timothycarambat <rambat1010@gmail.com>
2025-01-27 16:17:28 -0800

    Add ability to disable default agent skills

--- frontend/src/pages/Admin/Agents/DefaultSkillPanel/index.jsx
+++ frontend/src/pages/Admin/Agents/DefaultSkillPanel/index.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import { DefaultBadge } from "../Badges/default";
 
-export default function DefaultSkillPanel({ title, description, image, icon }) {
+export default function DefaultSkillPanel({
+  title,
+  description,
+  image,
+  icon,
+  enabled = true,
+  toggleSkill,
+  skill,
+}) {
   return (
     <div className="p-2">
       <div className="flex flex-col gap-y-[18px] max-w-[500px]">
@@ -21,10 +29,26 @@ export default function DefaultSkillPanel({ title, description, image, icon }) {
             </label>
             <DefaultBadge title={title} />
           </div>
+          <label
+            className={`border-none relative inline-flex items-center ml-auto cursor-pointer`}
+          >
+            <input
+              type="checkbox"
+              className="peer sr-only"
+              checked={enabled}
+              onChange={() => toggleSkill(skill)}
+            />
+            <div className="peer-disabled:opacity-50 pointer-events-none peer h-6 w-11 rounded-full bg-[#CFCFD0] after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:shadow-xl after:border-none after:bg-white after:box-shadow-md after:transition-all after:content-[''] peer-checked:bg-[#32D583] peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-transparent"></div>
+            <span className="ml-3 text-sm font-medium"></span>
+          </label>
         </div>
         <img src={image} alt={title} className="w-full rounded-md" />
         <p className="text-theme-text-secondary text-opacity-60 text-xs font-medium py-1.5">
           {description}
+          <br />
+          <br />
+          By default, this skill is enabled, but you can disable it if you don't
+          want it to be available to the agent.
         </p>
       </div>
     </div>
