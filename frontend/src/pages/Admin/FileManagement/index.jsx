e8c25036ef3379a30d5fb0bb46257231cde3eeba
Amit <amit.raina@saal.ai>
2025-01-31 20:03:30 +0530

    DAD-49 | Modal Size Width Increased

--- frontend/src/pages/Admin/FileManagement/index.jsx
+++ frontend/src/pages/Admin/FileManagement/index.jsx
@@ -293,7 +293,7 @@ function DocumentContentModal({ closeModal, docId = null, docName = "" }) {
 
   return (
     <div className="fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50 flex items-center justify-center">
-      <div className="relative w-full max-w-2xl bg-theme-bg-secondary rounded-lg shadow border-2 border-theme-modal-border custom-theme-bg-tertiary border-none">
+      <div className="relative w-full max-w-6xl bg-theme-bg-secondary rounded-lg shadow border-2 border-theme-modal-border custom-theme-bg-tertiary border-none">
         <div className="relative p-6 border-b rounded-t border-theme-modal-border">
           <div className="w-full flex gap-x-2 items-center pr-6">
             <h3 className="text-xl font-semibold text-white overflow-hidden overflow-ellipsis whitespace-nowrap custom-text-secondary">
