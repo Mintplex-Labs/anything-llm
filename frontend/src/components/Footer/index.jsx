7f6b630dbbed1e34313db2284e0b5e7fee38b645
HBS-AI <hbsaicreation@gmail.com>
2025-02-01 16:05:59 +0500

    Update index.jsx

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -1,9 +1,6 @@
 import System from "@/models/system";
 import paths from "@/utils/paths";
 import {
-  BookOpen,
-  DiscordLogo,
-  GithubLogo,
   Briefcase,
   Envelope,
   Globe,
@@ -19,9 +16,6 @@ import { Link } from "react-router-dom";
 
 export const MAX_ICONS = 3;
 export const ICON_COMPONENTS = {
-  BookOpen: BookOpen,
-  DiscordLogo: DiscordLogo,
-  GithubLogo: GithubLogo,
   Envelope: Envelope,
   LinkSimple: LinkSimple,
   HouseLine: HouseLine,
@@ -47,60 +41,13 @@ export default function Footer() {
 
   if (!Array.isArray(footerData) || footerData.length === 0) {
     return (
-      <div className="flex justify-center mb-2">
-        <div className="flex space-x-4">
-          <div className="flex w-fit">
-            <Link
-              to={paths.github()}
-              target="_blank"
-              rel="noreferrer"
-              className="transition-all duration-300 p-2 rounded-full bg-theme-sidebar-footer-icon hover:bg-theme-sidebar-footer-icon-hover"
-              aria-label="Find us on Github"
-              data-tooltip-id="footer-item"
-              data-tooltip-content="View source code on Github"
-            >
-              <GithubLogo
-                weight="fill"
-                className="h-5 w-5"
-                color="var(--theme-sidebar-footer-icon-fill)"
-              />
-            </Link>
-          </div>
-          <div className="flex w-fit">
-            <Link
-              to={paths.docs()}
-              target="_blank"
-              rel="noreferrer"
-              className="transition-all duration-300 p-2 rounded-full bg-theme-sidebar-footer-icon hover:bg-theme-sidebar-footer-icon-hover"
-              aria-label="Docs"
-              data-tooltip-id="footer-item"
-              data-tooltip-content="Open AnythingLLM help docs"
-            >
-              <BookOpen
-                weight="fill"
-                className="h-5 w-5"
-                color="var(--theme-sidebar-footer-icon-fill)"
-              />
-            </Link>
-          </div>
-          <div className="flex w-fit">
-            <Link
-              to={paths.discord()}
-              target="_blank"
-              rel="noreferrer"
-              className="transition-all duration-300 p-2 rounded-full bg-theme-sidebar-footer-icon hover:bg-theme-sidebar-footer-icon-hover"
-              aria-label="Join our Discord server"
-              data-tooltip-id="footer-item"
-              data-tooltip-content="Join the AnythingLLM Discord"
-            >
-              <DiscordLogo
-                weight="fill"
-                className="h-5 w-5"
-                color="var(--theme-sidebar-footer-icon-fill)"
-              />
-            </Link>
-          </div>
-          {!isMobile && <SettingsButton />}
+      <div className="flex justify-center items-center mb-2">
+        <div className="flex items-center">
+          {!isMobile && (
+            <div className="flex w-fit">
+              <SettingsButton />
+            </div>
+          )}
         </div>
         <Tooltip
           id="footer-item"
@@ -113,15 +60,15 @@ export default function Footer() {
   }
 
   return (
-    <div className="flex justify-center mb-2">
-      <div className="flex space-x-4">
+    <div className="flex justify-center items-center mb-2">
+      <div className="flex items-center space-x-4">
         {footerData.map((item, index) => (
           <a
             key={index}
             href={item.url}
             target="_blank"
             rel="noreferrer"
-            className="transition-all duration-300 flex w-fit h-fit p-2 p-2 rounded-full bg-theme-sidebar-footer-icon hover:bg-theme-sidebar-footer-icon-hover hover:border-slate-100"
+            className="transition-all duration-300 flex w-fit h-fit p-2 rounded-full bg-theme-sidebar-footer-icon hover:bg-theme-sidebar-footer-icon-hover hover:border-slate-100"
           >
             {React.createElement(
               ICON_COMPONENTS?.[item.icon] ?? ICON_COMPONENTS.Info,
@@ -133,7 +80,11 @@ export default function Footer() {
             )}
           </a>
         ))}
-        {!isMobile && <SettingsButton />}
+        {!isMobile && (
+          <div className="flex w-fit">
+            <SettingsButton />
+          </div>
+        )}
       </div>
       <Tooltip
         id="footer-item"
