53efb6c995cbc52ff6e676de12e2fca6f84e0505
timothycarambat <rambat1010@gmail.com>
2025-02-12 16:29:46 -0800

    Patch code block header oddity resolves #3117

--- frontend/src/utils/chat/markdown.js
+++ frontend/src/utils/chat/markdown.js
@@ -24,9 +24,9 @@ const markdown = markdownIt({
               <div class="flex gap-2">
                 <code class="text-xs">${lang || ""}</code>
               </div>
-              <button data-code-snippet data-code="code-${uuid}" class="flex items-center gap-x-2">
-                <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>
-                <p>Copy code</p>
+              <button data-code-snippet data-code="code-${uuid}" class="flex items-center gap-x-1">
+                <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-3 w-3" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>
+                <p class="text-xs" style="margin: 0px;padding: 0px;">Copy block</p>
               </button>
             </div>
           <pre class="whitespace-pre-wrap">` +
@@ -40,9 +40,9 @@ const markdown = markdownIt({
       `<div class="whitespace-pre-line w-full hljs ${theme} light:border-solid light:border light:border-gray-700 rounded-lg px-4 pb-4 relative font-mono font-normal text-sm text-slate-200">
         <div class="w-full flex items-center absolute top-0 left-0 text-slate-200 bg-stone-800 px-4 py-2 text-xs font-sans justify-between rounded-t-md">
           <div class="flex gap-2"><code class="text-xs"></code></div>
-          <button data-code-snippet data-code="code-${uuid}" class="flex items-center gap-x-2">
-            <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>
-            <p>Copy code</p>
+          <button data-code-snippet data-code="code-${uuid}" class="flex items-center gap-x-1">
+            <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-3 w-3" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>
+            <p class="text-xs" style="margin: 0px;padding: 0px;">Copy block</p>
           </button>
         </div>
       <pre class="whitespace-pre-wrap">` +
