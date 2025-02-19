723360d88b2d164bc15c0adad7d4da34ab519297
timothycarambat <rambat1010@gmail.com>
2025-02-18 21:56:31 -0800

    docs: update jsdoc comment for hook fix: apply chat alignment to homepage chat

--- frontend/src/hooks/useChatMessageAlignment.js
+++ frontend/src/hooks/useChatMessageAlignment.js
@@ -3,7 +3,7 @@ const ALIGNMENT_STORAGE_KEY = "anythingllm-chat-message-alignment";
 
 /**
  * Store the message alignment in localStorage as well as provide a function to get the alignment of a message via role.
- * @returns {{msgDirection: 'left'|'left_right', setMsgDirection: (direction: string) => void, getMessageAlignment: (role: string) => {isLeftToRight: boolean, directionCls: string}}} - The message direction and the class name for the direction.
+ * @returns {{msgDirection: 'left'|'left_right', setMsgDirection: (direction: string) => void, getMessageAlignment: (role: string) => string}} - The message direction and the class name for the direction.
  */
 export function useChatMessageAlignment() {
   const [msgDirection, setMsgDirection] = useState(
