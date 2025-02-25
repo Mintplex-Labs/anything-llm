d1354caccbfedb47972295a781a33508d3d445db
Skanda Kaashyap <skandakk@gmail.com>
2025-02-25 12:52:17 -0800

    [FEAT] Add claude-3-7 (#3337)
    * add claude 3-7 sonnet
    
    * made all the changes everywhere
    
    * add 3-7-sonnet-latest model
    
    * lint
    
    ---------
    
    Co-authored-by: shatfield4 <seanhatfield5@gmail.com>

--- server/utils/AiProviders/anthropic/index.js
+++ server/utils/AiProviders/anthropic/index.js
@@ -58,6 +58,8 @@ class AnthropicLLM {
       "claude-3-5-sonnet-latest",
       "claude-3-5-sonnet-20241022",
       "claude-3-5-sonnet-20240620",
+      "claude-3-7-sonnet-20250219",
+      "claude-3-7-sonnet-latest",
     ];
     return validModels.includes(modelName);
   }
