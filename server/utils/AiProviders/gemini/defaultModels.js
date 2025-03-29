847a31a7ea5debc25e3326a940054856aab37460
DreamerC <dreamerwolf.tw@gmail.com>
2025-03-30 05:34:54 +0800

    Update defaultModels.js
    20250330 model update

--- server/utils/AiProviders/gemini/defaultModels.js
+++ server/utils/AiProviders/gemini/defaultModels.js
@@ -1,29 +1,88 @@
 const { MODEL_MAP } = require("../modelMap");
 
+// Google Gemini API , list models : https://generativelanguage.googleapis.com/(v1|v1beta)/models?key=$(APIKey)
+// 20250330 update
+
 const stableModels = [
-  "gemini-pro",
-  "gemini-1.0-pro",
+  "gemini-1.0-pro-vision-latest",
+  "gemini-pro-vision",
   "gemini-1.5-pro-latest",
+  "gemini-1.5-pro-001",
+  "gemini-1.5-pro-002",
+  "gemini-1.5-pro",
   "gemini-1.5-flash-latest",
+  "gemini-1.5-flash-001",
+  "gemini-1.5-flash-001-tuning",
+  "gemini-1.5-flash",
+  "gemini-1.5-flash-002",
+  "gemini-1.5-flash-8b",
+  "gemini-1.5-flash-8b-001",
+  "gemini-1.5-flash-8b-latest",
+  "gemini-2.0-flash",
+  "gemini-2.0-flash-001",
+  "gemini-2.0-flash-lite-001",
+  "gemini-2.0-flash-lite",
+  "embedding-001",
+  "text-embedding-004",
 ];
 
 const experimentalModels = [
-  "gemini-1.5-pro-exp-0801",
-  "gemini-1.5-pro-exp-0827",
-  "gemini-1.5-flash-exp-0827",
+  "chat-bison-001",
+  "text-bison-001",
+  "embedding-gecko-001",
   "gemini-1.5-flash-8b-exp-0827",
-  "gemini-exp-1114",
-  "gemini-exp-1121",
-  "gemini-exp-1206",
-  "learnlm-1.5-pro-experimental",
+  "gemini-1.5-flash-8b-exp-0924",
+  "gemini-2.5-pro-exp-03-25",
   "gemini-2.0-flash-exp",
+  "gemini-2.0-flash-exp-image-generation",
+  "gemini-2.0-flash-lite-preview-02-05",
+  "gemini-2.0-flash-lite-preview",
+  "gemini-2.0-pro-exp",
+  "gemini-2.0-pro-exp-02-05",
+  "gemini-exp-1206",
+  "gemini-2.0-flash-thinking-exp-01-21",
+  "gemini-2.0-flash-thinking-exp",
+  "gemini-2.0-flash-thinking-exp-1219",
+  "learnlm-1.5-pro-experimental",
+  "gemma-3-27b-it",
+  "gemini-embedding-exp-03-07",
+  "gemini-embedding-exp",
+  "aqa",
+  "imagen-3.0-generate-002"
 ];
 
 // There are some models that are only available in the v1beta API
 // and some models that are only available in the v1 API
 // generally, v1beta models have `exp` in the name, but not always
 // so we check for both against a static list as well.
-const v1BetaModels = ["gemini-1.5-pro-latest", "gemini-1.5-flash-latest", "gemma-3-27b-it"];
+
+const v1BetaModels = [
+  "chat-bison-001",
+  "text-bison-001",
+  "embedding-gecko-001",
+  "gemini-1.5-pro-latest",
+  "gemini-1.5-flash-latest",
+  "gemini-1.5-flash-8b-latest",
+  "gemini-1.5-flash-8b-exp-0827",
+  "gemini-1.5-flash-8b-exp-0924",
+  "gemini-2.5-pro-exp-03-25",
+  "gemini-2.0-flash-exp",
+  "gemini-2.0-flash-exp-image-generation",
+  "gemini-2.0-flash-lite-preview-02-05",
+  "gemini-2.0-flash-lite-preview",
+  "gemini-2.0-pro-exp",
+  "gemini-2.0-pro-exp-02-05",
+  "gemini-exp-1206",
+  "gemini-2.0-flash-thinking-exp-01-21",
+  "gemini-2.0-flash-thinking-exp",
+  "gemini-2.0-flash-thinking-exp-1219",
+  "learnlm-1.5-pro-experimental",
+  "gemma-3-27b-it",
+  "gemini-embedding-exp-03-07",
+  "gemini-embedding-exp",
+  "aqa",
+  "imagen-3.0-generate-002"
+];
 
 const defaultGeminiModels = [
   ...stableModels.map((model) => ({
