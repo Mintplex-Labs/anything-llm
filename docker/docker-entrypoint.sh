30a388efbe24ac494047cf827df1ef94326258d8
Nic Raboy <nraboy@gmail.com>
2025-04-04 14:47:02 -0700

    Allow User to Change UID/GID Values
    In the current version, the documentation advertises changing the UID and GID values in the `.env` file, however it doesn't appear it actually does anything when inspecting the codebase.
    
    The current codebase allows you to set the UID and GID values when building the image, which isn't particularly useful when deploying containers.
    
    My proposed changes allow for optionally changing the UID and GID values at runtime. To make this possible, the entire `Dockerfile` is ran as the `root` user. In the `docker-entrypoint.sh` file, which is ran at container runtime, if the UID or GID values are set in the environment variables, then they will be changed for the `anythingllm` user. The application is started as the `anythingllm` user through `gosu` rather than continuing as `root` user.
    
    This proposed change is valuable to avoid permission issues on mapped volumes.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-{
-  cd /app/server/ &&
-    npx prisma generate --schema=./prisma/schema.prisma &&
-    npx prisma migrate deploy --schema=./prisma/schema.prisma &&
-    node /app/server/index.js
-} &
-{ node /app/collector/index.js; } &
+
+if [ ! -z "${UID}" ]; then
+  if [ ! "$(id -u anythingllm)" -eq "${UID}" ]; then
+    usermod -o -u "${UID}" anythingllm
+  fi
+fi
+
+if [ ! -z "${GID}" ]; then
+  if [ ! "$(id -g anythingllm)" -eq "${GID}" ]; then
+    groupmod -g "${GID}" anythingllm
+  fi
+fi
+
+chown -R anythingllm:anythingllm /app
+chown -R anythingllm:anythingllm "${STORAGE_DIR}"
+
+gosu anythingllm bash -c "cd /app/server && npx prisma generate --schema=./prisma/schema.prisma && npx prisma migrate deploy --schema=./prisma/schema.prisma && node /app/server/index.js" &
+
+gosu anythingllm bash -c "node /app/collector/index.js" &
+
 wait -n
 exit $?
