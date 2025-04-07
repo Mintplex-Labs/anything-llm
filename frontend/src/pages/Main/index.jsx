927fc6326bdc62f02712069ef09a84fa7ebfb356
shatfield4 <seanhatfield5@gmail.com>
2025-04-07 14:42:27 -0700

    conditionally render legacy default chat page when in multiuser

--- frontend/src/pages/Main/index.jsx
+++ frontend/src/pages/Main/index.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PasswordModal, { usePasswordModal } from "@/components/Modals/Password";
 import { FullScreenLoader } from "@/components/Preloader";
 import Home from "./Home";
+import DefaultChatContainer from "@/components/DefaultChat";
+import { isMobile } from "react-device-detect";
+import Sidebar from "@/components/Sidebar";
+import System from "@/models/system";
 
 export default function Main() {
   const { loading, requiresAuth, mode } = usePasswordModal();
+  const [ismultiUser, setIsMultiUser] = useState(false);
+
+  useEffect(() => {
+    async function checkMultiUserMode() {
+      const isMultiUser = await System.isMultiUserMode();
+      setIsMultiUser(isMultiUser);
+    }
+    checkMultiUserMode();
+  }, []);
 
   if (loading) return <FullScreenLoader />;
   if (requiresAuth !== false) {
@@ -13,7 +26,14 @@ export default function Main() {
 
   return (
     <div className="w-screen h-screen overflow-hidden bg-theme-bg-container flex">
-      <Home />
+      {ismultiUser ? (
+        <>
+          {!isMobile && <Sidebar />}
+          <DefaultChatContainer />
+        </>
+      ) : (
+        <Home />
+      )}
     </div>
   );
 }
