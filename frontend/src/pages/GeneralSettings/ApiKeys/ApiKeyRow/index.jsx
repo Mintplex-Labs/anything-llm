da11f3b997f90f410e6ed1e58e6bc46c847260b2
timothycarambat <rambat1010@gmail.com>
2025-05-14 16:05:10 -0700

    patch api key bad ref.current on add post removal

--- frontend/src/pages/GeneralSettings/ApiKeys/ApiKeyRow/index.jsx
+++ frontend/src/pages/GeneralSettings/ApiKeys/ApiKeyRow/index.jsx
@@ -1,12 +1,11 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import Admin from "@/models/admin";
 import showToast from "@/utils/toast";
 import { Trash } from "@phosphor-icons/react";
 import { userFromStorage } from "@/utils/request";
 import System from "@/models/system";
 
-export default function ApiKeyRow({ apiKey }) {
-  const rowRef = useRef(null);
+export default function ApiKeyRow({ apiKey, removeApiKey }) {
   const [copied, setCopied] = useState(false);
   const handleDelete = async () => {
     if (
@@ -15,14 +14,12 @@ export default function ApiKeyRow({ apiKey }) {
       )
     )
       return false;
-    if (rowRef?.current) {
-      rowRef.current.remove();
-    }
 
     const user = userFromStorage();
     const Model = !!user ? Admin : System;
     await Model.deleteApiKey(apiKey.id);
     showToast("API Key permanently deleted", "info");
+    removeApiKey(apiKey.id);
   };
 
   const copyApiKey = () => {
@@ -44,10 +41,7 @@ export default function ApiKeyRow({ apiKey }) {
 
   return (
     <>
-      <tr
-        ref={rowRef}
-        className="bg-transparent text-white text-opacity-80 text-xs font-medium border-b border-white/10 h-10"
-      >
+      <tr className="bg-transparent text-white text-opacity-80 text-xs font-medium border-b border-white/10 h-10">
         <td scope="row" className="px-6 whitespace-nowrap">
           {apiKey.secret}
         </td>
