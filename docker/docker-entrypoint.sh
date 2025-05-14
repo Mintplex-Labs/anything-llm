00e1c54b4b60ad79b8d8dc3ce27e8e70c4e05262
timothycarambat <rambat1010@gmail.com>
2025-05-14 11:00:50 -0700

    update newline printing

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,12 +4,14 @@
 if [ -z "$STORAGE_DIR" ]; then
     echo "================================================================"
     echo "⚠️  ⚠️  ⚠️  WARNING: STORAGE_DIR environment variable is not set! ⚠️  ⚠️  ⚠️"
+    echo ""
     echo "Not setting this will result in data loss on container restart since"
     echo "the application will not have a persistent storage location."
     echo "It can also result in weird errors in various parts of the application."
-    echo "\n\n"
+    echo ""
     echo "Please run the container with the official docker command at"
     echo "https://docs.anythingllm.com/installation-docker/quickstart"
+    echo ""
     echo "⚠️  ⚠️  ⚠️  WARNING: STORAGE_DIR environment variable is not set! ⚠️  ⚠️  ⚠️"
     echo "================================================================"
 fi
