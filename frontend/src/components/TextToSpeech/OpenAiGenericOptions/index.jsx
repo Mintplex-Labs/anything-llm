a0df99de081b7c35d44f553061b9377cbb69cb95
timothycarambat <rambat1010@gmail.com>
2025-07-17 10:26:25 -0700

    Add `model` field parameter for generic OpenAI TTS resolves #4165

--- frontend/src/components/TextToSpeech/OpenAiGenericOptions/index.jsx
+++ frontend/src/components/TextToSpeech/OpenAiGenericOptions/index.jsx
@@ -5,7 +5,7 @@ export default function OpenAiGenericTextToSpeechOptions({ settings }) {
     <div className="w-full flex flex-col gap-y-7">
       <div className="flex gap-x-4">
         <div className="flex flex-col w-60">
-          <div className="flex justify-between items-center mb-2">
+          <div className="flex justify-between items-start mb-2">
             <label className="text-white text-sm font-semibold">Base URL</label>
           </div>
           <input
@@ -23,9 +23,8 @@ export default function OpenAiGenericTextToSpeechOptions({ settings }) {
             will generate TTS responses from.
           </p>
         </div>
-
         <div className="flex flex-col w-60">
-          <label className="text-white text-sm font-semibold block mb-3">
+          <label className="text-white text-sm font-semibold block mb-2">
             API Key
           </label>
           <input
@@ -42,6 +41,28 @@ export default function OpenAiGenericTextToSpeechOptions({ settings }) {
           <p className="text-xs leading-[18px] font-base text-white text-opacity-60 mt-2">
             Some TTS services require an API key to generate TTS responses -
             this is optional if your service does not require one.
+          </p>
+        </div>
+      </div>
+      <div className="flex gap-x-4">
+        <div className="flex flex-col w-60">
+          <label className="text-white text-sm font-semibold block mb-3">
+            TTS Model
+          </label>
+          <input
+            type="text"
+            name="TTSOpenAICompatibleModel"
+            className="border-none bg-theme-settings-input-bg text-white placeholder:text-theme-settings-input-placeholder text-sm rounded-lg focus:outline-primary-button active:outline-primary-button outline-none block w-full p-2.5"
+            placeholder="Your TTS model identifier"
+            defaultValue={settings?.TTSOpenAICompatibleModel}
+            required={true}
+            autoComplete="off"
+            spellCheck={false}
+          />
+          <p className="text-xs leading-[18px] font-base text-white text-opacity-60 mt-2">
+            Most TTS services will have several models available. This is the{" "}
+            <code>model</code> parameter you will use to select the model you
+            want to use. Note: This is not the same as the voice model.
           </p>
         </div>
         <div className="flex flex-col w-60">
