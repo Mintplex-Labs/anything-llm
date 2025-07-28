6c957cf756635bd69b35e0f93482d015f15da7bd
timothycarambat <rambat1010@gmail.com>
2025-07-28 08:28:34 -0700

    add back removal of ending curly brace

--- server/utils/agents/aibitat/plugins/sql-agent/SQLConnectors/Postgresql.js
+++ server/utils/agents/aibitat/plugins/sql-agent/SQLConnectors/Postgresql.js
@@ -61,5 +61,6 @@ class PostgresSQLConnector {
   getTableSchemaSql(table_name) {
     return ` select column_name, data_type, character_maximum_length, column_default, is_nullable from INFORMATION_SCHEMA.COLUMNS where table_name = '${table_name}' AND table_schema = '${this.schema}'`;
   }
+}
 
 module.exports.PostgresSQLConnector = PostgresSQLConnector;
