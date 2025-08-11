bda9562d61e8124d41d1e6ae92db805bb44ea5cb
Ashika Anand Babu <Ashika.makam@gmail.com>
2025-08-11 15:14:27 -0700

    Update Security UI to match all other Settings

--- frontend/src/locales/en/common.js
+++ frontend/src/locales/en/common.js
@@ -682,17 +682,20 @@ const TRANSLATIONS = {
     },
   },
 
-  multi: {
-    title: "Multi-User Mode",
-    description:
-      "Set up your instance to support your team by activating Multi-User Mode.",
-    enable: {
-      "is-enable": "Multi-User Mode is Enabled",
-      enable: "Enable Multi-User Mode",
+  security: {
+    title: "Security",
+    multiuser: {
+      title: "Multi-User Mode",
       description:
-        "By default, you will be the only admin. As an admin you will need to create accounts for all new users or admins. Do not lose your password as only an Admin user can reset passwords.",
-      username: "Admin account username",
-      password: "Admin account password",
+        "Set up your instance to support your team by activating Multi-User Mode.",
+      enable: {
+        "is-enable": "Multi-User Mode is Enabled",
+        enable: "Enable Multi-User Mode",
+        description:
+          "By default, you will be the only admin. As an admin you will need to create accounts for all new users or admins. Do not lose your password as only an Admin user can reset passwords.",
+        username: "Admin account username",
+        password: "Admin account password",
+      },
     },
     password: {
       title: "Password Protection",
