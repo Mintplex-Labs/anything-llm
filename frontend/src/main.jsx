e354b7074038ecf5091c8ec0afd1ad6c8edfb9c3
akigogikar <agogikar@linbeck.com>
2025-08-18 00:25:54 +0530

    feat(frontend): add OneNew component utility styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "@/App.jsx";
 import "@/index.css";
+import "./styles/onenew-components.css";
 const isDev = process.env.NODE_ENV !== "production";
 const REACTWRAP = isDev ? React.Fragment : React.StrictMode;
 
