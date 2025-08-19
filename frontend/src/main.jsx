27ac1cd2eb6119baf7b30e912c248bc237f0931c
akigogikar <agogikar@linbeck.com>
2025-08-19 21:03:03 +0530

    feat: add global focus visible outline

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import App from "@/App.jsx";
 import "@/index.css";
 import "./styles/onenew-components.css";
 import "./styles/forms.css";
+import "./styles/a11y.css";
 const isDev = process.env.NODE_ENV !== "production";
 const REACTWRAP = isDev ? React.Fragment : React.StrictMode;
 
