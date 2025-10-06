a72c71268a9915e2f1a909498e30740b6660b36f
timothycarambat <rambat1010@gmail.com>
2025-10-06 15:59:10 -0700

    add comp on desktop only

--- frontend/src/components/Sidebar/index.jsx
+++ frontend/src/components/Sidebar/index.jsx
@@ -73,6 +73,7 @@ export default function Sidebar() {
         </div>
         {showingNewWsModal && <NewWorkspaceModal hideModal={hideNewWsModal} />}
       </div>
+      <WorkspaceAndThreadTooltips />
     </>
   );
 }
