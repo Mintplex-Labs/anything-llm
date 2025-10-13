7550245910b6b3d252686a08d3e0f4b29310f7f9
naaa760 <neh6a683@gmail.com>
2025-10-14 03:01:07 +0530

    feat: export N1nProvider for agent system

--- server/utils/agents/aibitat/providers/index.js
+++ server/utils/agents/aibitat/providers/index.js
@@ -26,6 +26,7 @@ const DellProAiStudioProvider = require("./dellProAiStudio.js");
 const MoonshotAiProvider = require("./moonshotAi.js");
 const CometApiProvider = require("./cometapi.js");
 const FoundryProvider = require("./foundry.js");
+const N1nProvider = require("./n1n.js");
 
 module.exports = {
   OpenAIProvider,
@@ -56,4 +57,5 @@ module.exports = {
   DellProAiStudioProvider,
   MoonshotAiProvider,
   FoundryProvider,
+  N1nProvider,
 };
