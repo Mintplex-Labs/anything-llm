3d52ff6877320befc0c2c9d3c5f9cecda2614f94
shatfield4 <seanhatfield5@gmail.com>
2025-12-05 16:32:11 -0800

    migrate weaviate to class

--- server/utils/helpers/index.js
+++ server/utils/helpers/index.js
@@ -98,7 +98,7 @@ function getVectorDbClass(getExactly = null) {
       return LanceDb;
     case "weaviate":
       const { Weaviate } = require("../vectorDbProviders/weaviate");
-      return Weaviate;
+      return new Weaviate();
     case "qdrant":
       const { QDrant } = require("../vectorDbProviders/qdrant");
       return QDrant;
