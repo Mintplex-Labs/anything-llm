8e0186f9accc2d2b4f90ac1f60513183f4077a2e
Marcello Fitton <106866560+angelplusultra@users.noreply.github.com>
2025-12-10 12:40:34 -0800

    Fix stale user permissions in UI by refreshing user data on app load (#4751)
    * add refresh user functionality
    
    * prettier
    
    * add eslint disable comment for exhaustive-deps warning in AuthContext to stop nagging about navigate func
    
    * remove unused imports and fix typo
    
    * handle unsafe parse of undefined for in-session user deleted
    
    ---------
    
    Co-authored-by: Timothy Carambat <rambat1010@gmail.com>

--- frontend/src/AuthContext.jsx
+++ frontend/src/AuthContext.jsx
@@ -1,20 +1,31 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
 import {
   AUTH_TIMESTAMP,
   AUTH_TOKEN,
   AUTH_USER,
   USER_PROMPT_INPUT_MAP,
 } from "@/utils/constants";
+import System from "./models/system";
+import { useNavigate } from "react-router-dom";
+import { safeJsonParse } from "@/utils/request";
 
 export const AuthContext = createContext(null);
 export function AuthProvider(props) {
   const localUser = localStorage.getItem(AUTH_USER);
   const localAuthToken = localStorage.getItem(AUTH_TOKEN);
   const [store, setStore] = useState({
-    user: localUser ? JSON.parse(localUser) : null,
+    user: localUser ? safeJsonParse(localUser, null) : null,
     authToken: localAuthToken ? localAuthToken : null,
   });
 
+  const navigate = useNavigate();
+
+  /* NOTE:
+   * 1. There's no reason for these helper functions to be stateful. They could
+   * just be regular funcs or methods on a basic object.
+   * 2. These actions are not being invoked anywhere in the
+   * codebase, dead code.
+   */
   const [actions] = useState({
     updateUser: (user, authToken = "") => {
       localStorage.setItem(AUTH_USER, JSON.stringify(user));
@@ -29,6 +40,29 @@ export function AuthProvider(props) {
       setStore({ user: null, authToken: null });
     },
   });
+
+  // On initial mount and whenever the token changes fetch a new user object
+  useEffect(() => {
+    if (store.authToken) {
+      System.refreshUser()
+        .then(({ user }) => {
+          localStorage.setItem(AUTH_USER, JSON.stringify(user));
+          setStore((prev) => ({
+            ...prev,
+            user,
+          }));
+        })
+        .catch(() => {
+          localStorage.removeItem(AUTH_USER);
+          localStorage.removeItem(AUTH_TOKEN);
+          localStorage.removeItem(AUTH_TIMESTAMP);
+          localStorage.removeItem(USER_PROMPT_INPUT_MAP);
+          setStore({ user: null, authToken: null });
+          navigate("/login");
+        });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [store.authToken]);
 
   return (
     <AuthContext.Provider value={{ store, actions }}>
