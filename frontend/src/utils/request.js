8904bf24f8e1aaa6d5297de7832f6b69320619ad
Timothy Carambat <rambat1010@gmail.com>
2025-12-11 16:04:13 -0800

    add guard clause and return null for `userFromStorage`

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -5,7 +5,7 @@ import { AUTH_TOKEN, AUTH_USER } from "./constants";
 export function userFromStorage() {
   const userString = window.localStorage.getItem(AUTH_USER);
   if (!userString) return null;
-  return safeJsonParse(userString, {});
+  return safeJsonParse(userString, null);
 }
 
 export function baseHeaders(providedToken = null) {
@@ -17,6 +17,7 @@ export function baseHeaders(providedToken = null) {
 
 export function safeJsonParse(jsonString, fallback = null) {
   try {
+    if (jsonString === null || jsonString === undefined) return fallback;
     return JSON.parse(jsonString);
   } catch {}
   return fallback;
