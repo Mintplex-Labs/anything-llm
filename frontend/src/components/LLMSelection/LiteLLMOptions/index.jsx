ff7cb17e34cedc97fb465303baec9f0bdee176ce
Timothy Carambat <rambat1010@gmail.com>
2026-01-14 15:55:26 -0800

    Improved DMR support (#4863)
    * Improve DMR support
    - Autodetect models installed
    - Grab all models from hub.docker to show available
    - UI to handle render,search, install, and management of models
    - Support functionality for chat, stream, and agentic calls
    
    * forgot files
    
    * fix loader circle being too large
    fix tooltip width command
    adjust location of docker installer open for web platform
    
    * adjust imports

--- frontend/src/components/LLMSelection/LiteLLMOptions/index.jsx
+++ frontend/src/components/LLMSelection/LiteLLMOptions/index.jsx
@@ -34,7 +34,7 @@ export default function LiteLLMOptions({ settings }) {
         />
         <div className="flex flex-col w-60">
           <label className="text-white text-sm font-semibold block mb-3">
-            Token context window
+            Model context window
           </label>
           <input
             type="number"
