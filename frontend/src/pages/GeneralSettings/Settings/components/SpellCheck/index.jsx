74c64241822fe792edc43ca9e4b4b252f59ad832
Marcello Fitton <macfittondev@gmail.com>
2026-01-28 15:14:08 -0800

    remove unused imports

--- frontend/src/pages/GeneralSettings/Settings/components/SpellCheck/index.jsx
+++ frontend/src/pages/GeneralSettings/Settings/components/SpellCheck/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Appearance from "@/models/appearance";
 import { useTranslation } from "react-i18next";
 
