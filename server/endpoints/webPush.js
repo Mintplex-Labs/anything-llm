ede98bc17c9d2ade1a70f3f283db4f7bea8e55d0
Timothy Carambat <rambat1010@gmail.com>
2026-02-02 10:53:09 -0800

    cleanup for web-push bootstrapping

--- server/endpoints/webPush.js
+++ server/endpoints/webPush.js
@@ -22,33 +22,6 @@ function webPushEndpoints(app) {
     const publicKey = pushNotificationService.publicVapidKey;
     response.status(200).json({ publicKey });
   });
-
-  // TODO: Remove this endpoint after testing
-  app.get("/web-push/test", [validatedRequest], async (_request, response) => {
-    if (response.locals.multiUserMode) {
-      console.log("Sending notification to user");
-      const { user } = response.locals;
-      console.log(JSON.stringify(user, null, 2));
-      pushNotificationService.sendNotification({
-        to: user.id,
-        payload: {
-          title: `Hello, ${user.username}`,
-          body: `This is a test notification for ${user.username} from AnythingLLM`,
-        },
-      });
-      return response.status(200).json({});
-    }
-
-    console.log("Sending notification to primary user");
-    pushNotificationService.sendNotification({
-      to: "primary",
-      payload: {
-        title: "Test Notification",
-        body: "This is a test notification",
-      },
-    });
-    response.status(200).json({});
-  });
 }
 
 module.exports = { webPushEndpoints };
