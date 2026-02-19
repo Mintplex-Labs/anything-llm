e025df9b87e25013fb4205b6c229cee8c7359361
Marcello Fitton <106866560+angelplusultra@users.noreply.github.com>
2026-02-19 12:37:13 -0800

    feat: dedicated dark theme option with system preference support (#5007)
    * implement OS level theme switching and dark mode option
    
    * simplify
    
    * fix logo bug in login | place back useTheme comment
    
    ---------
    
    Co-authored-by: shatfield4 <seanhatfield5@gmail.com>
    Co-authored-by: Timothy Carambat <rambat1010@gmail.com>

--- frontend/src/LogoContext.jsx
+++ frontend/src/LogoContext.jsx
@@ -6,18 +6,21 @@ import DefaultLoginLogoDark from "./media/illustrations/login-logo-light.svg";
 import System from "./models/system";
 
 export const REFETCH_LOGO_EVENT = "refetch-logo";
+
+function isLightMode() {
+  return document.documentElement.getAttribute("data-theme") === "light";
+}
 export const LogoContext = createContext();
 
 export function LogoProvider({ children }) {
   const [logo, setLogo] = useState("");
   const [loginLogo, setLoginLogo] = useState("");
   const [isCustomLogo, setIsCustomLogo] = useState(false);
-  const DefaultLoginLogo =
-    localStorage.getItem("theme") !== "default"
-      ? DefaultLoginLogoDark
-      : DefaultLoginLogoLight;
 
   async function fetchInstanceLogo() {
+    const DefaultLoginLogo = isLightMode()
+      ? DefaultLoginLogoDark
+      : DefaultLoginLogoLight;
     try {
       const { isCustomLogo, logoURL } = await System.fetchLogo();
       if (logoURL) {
@@ -25,16 +28,12 @@ export function LogoProvider({ children }) {
         setLoginLogo(isCustomLogo ? logoURL : DefaultLoginLogo);
         setIsCustomLogo(isCustomLogo);
       } else {
-        localStorage.getItem("theme") !== "default"
-          ? setLogo(AnythingLLMDark)
-          : setLogo(AnythingLLM);
+        isLightMode() ? setLogo(AnythingLLMDark) : setLogo(AnythingLLM);
         setLoginLogo(DefaultLoginLogo);
         setIsCustomLogo(false);
       }
     } catch (err) {
-      localStorage.getItem("theme") !== "default"
-        ? setLogo(AnythingLLMDark)
-        : setLogo(AnythingLLM);
+      isLightMode() ? setLogo(AnythingLLMDark) : setLogo(AnythingLLM);
       setLoginLogo(DefaultLoginLogo);
       setIsCustomLogo(false);
       console.error("Failed to fetch logo:", err);
