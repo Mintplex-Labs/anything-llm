de63b63c2c37ea4c89602eb5d5b7fd7774b2fd88
shatfield4 <seanhatfield5@gmail.com>
2026-03-06 10:31:53 -0800

    missing translations

--- frontend/src/components/WorkspaceChat/ChatContainer/PromptInput/StopGenerationButton/index.jsx
+++ frontend/src/components/WorkspaceChat/ChatContainer/PromptInput/StopGenerationButton/index.jsx
@@ -1,7 +1,9 @@
 import { ABORT_STREAM_EVENT } from "@/utils/chat";
 import { Tooltip } from "react-tooltip";
+import { useTranslation } from "react-i18next";
 
 export default function StopGenerationButton() {
+  const { t } = useTranslation();
   function emitHaltEvent() {
     window.dispatchEvent(new CustomEvent(ABORT_STREAM_EVENT));
   }
@@ -12,7 +14,7 @@ export default function StopGenerationButton() {
         type="button"
         onClick={emitHaltEvent}
         data-tooltip-id="stop-generation-button"
-        data-tooltip-content="Stop generating response"
+        data-tooltip-content={t("chat_window.stop_generating")}
         className="border-none inline-flex justify-center items-center rounded-full cursor-pointer w-8 h-8 bg-white light:bg-slate-800 hover:opacity-80 transition-opacity"
         aria-label="Stop generating"
       >
