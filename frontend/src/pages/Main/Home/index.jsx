3c40aefc36e8718eb27604ce472cee4520e3c6f9
Timothy Carambat <rambat1010@gmail.com>
2026-03-09 20:46:33 -0700

    container borders for dark/light mode compliance to designs

--- frontend/src/pages/Main/Home/index.jsx
+++ frontend/src/pages/Main/Home/index.jsx
@@ -277,7 +277,7 @@ function HomeContent({ workspace, setWorkspace, threadSlug, setThreadSlug }) {
   return (
     <div
       style={{ height: isMobile ? "100%" : "calc(100% - 32px)" }}
-      className="transition-all duration-500 relative md:ml-[2px] md:mr-[16px] md:my-[16px] md:rounded-[16px] bg-zinc-900 light:bg-white w-full h-full overflow-hidden"
+      className="transition-all duration-500 relative md:ml-[2px] md:mr-[16px] md:my-[16px] md:rounded-[16px] bg-zinc-900 light:bg-white w-full h-full overflow-hidden border-none light:border-solid light:border light:border-theme-modal-border"
     >
       {isMobile && <SidebarMobileHeader />}
       <TextSizeMenu />
