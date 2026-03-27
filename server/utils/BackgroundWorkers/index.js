03b1ec2ee21dd873b4971e056f25415a3d6d3119
Timothy Carambat <rambat1010@gmail.com>
2026-03-26 21:11:25 -0700

    reset timeout

--- server/utils/BackgroundWorkers/index.js
+++ server/utils/BackgroundWorkers/index.js
@@ -17,8 +17,7 @@ class BackgroundService {
     },
     {
       name: "cleanup-generated-files",
-      // timeout: "5m",
-      timeout: "10s",
+      timeout: "5m",
       interval: "8hr",
     },
   ];
