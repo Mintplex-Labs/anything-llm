89004f61bf22a6f490357cfd4fb0c6c5daeab96e
Timothy Carambat <rambat1010@gmail.com>
2026-03-26 21:41:45 -0700

    Txt creation (#5279)
    * wip
    
    * download card
    
    * UI for downloading
    
    * move to fs system with endpoint to pull files
    
    * refactor UI
    
    * final-pass
    
    * remove save-file-browser skill and refactor
    
    * remove fileDownload event
    
    * reset
    
    * reset file
    
    * reset timeout
    
    * wip
    
    * persist toggle
    
    * add arbitrary text creation file

--- server/utils/agents/aibitat/plugins/create-files/index.js
+++ server/utils/agents/aibitat/plugins/create-files/index.js
@@ -1,11 +1,12 @@
 const { CreatePptxPresentation } = require("./pptx/create-presentation.js");
+const { CreateTextFile } = require("./text/create-text-file.js");
 
 const createFilesAgent = {
   name: "create-files-agent",
   startupConfig: {
     params: {},
   },
-  plugin: [CreatePptxPresentation],
+  plugin: [CreatePptxPresentation, CreateTextFile],
 };
 
 module.exports = {
