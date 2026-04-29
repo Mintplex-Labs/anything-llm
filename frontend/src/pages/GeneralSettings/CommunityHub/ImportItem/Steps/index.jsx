4c945e5c51e4b8ab41bcea9a416904f0b1b1d483
Timothy Carambat <rambat1010@gmail.com>
2026-04-29 12:36:57 -0700

    remove bg to keep designs in spec

--- frontend/src/pages/GeneralSettings/CommunityHub/ImportItem/Steps/index.jsx
+++ frontend/src/pages/GeneralSettings/CommunityHub/ImportItem/Steps/index.jsx
@@ -66,7 +66,7 @@ export function CommunityHubImportItemLayout({ setStep, children }) {
       <Sidebar />
       <div
         style={{ height: isMobile ? "100%" : "calc(100% - 32px)" }}
-        className="relative md:ml-[2px] md:mr-[16px] md:my-[16px] md:rounded-[16px] bg-theme-bg-secondary w-full h-full overflow-y-scroll p-4 md:p-0"
+        className="relative md:ml-[2px] md:mr-[16px] md:my-[16px] md:rounded-[16px] w-full h-full overflow-y-scroll p-4 md:p-0"
       >
         {children(settings, setSettings, setStep)}
       </div>
