d1c45f5c6c6d185ee706546329351c1d33afef15
Sean Hatfield <seanhatfield5@gmail.com>
2026-08-18 11:21:19 -0700

    New AnythingLLM wordmark + fix logo theme resolution (#6138)
    * new AnythingLLM wordmark + fix logo theme resolution
    
    * update README + Open Computer wordmark
    
    * use new wordmark on onboarding home
    
    ---------
    
    Co-authored-by: Timothy Carambat <rambat1010@gmail.com>

--- server/endpoints/system.js
+++ server/endpoints/system.js
@@ -711,8 +711,7 @@ function systemEndpoints(app) {
 
   app.get("/system/logo", async function (request, response) {
     try {
-      const darkMode =
-        !request?.query?.theme || request?.query?.theme === "default";
+      const darkMode = request?.query?.theme !== "light";
       const defaultFilename = getDefaultFilename(darkMode);
       const logoPath = await determineLogoFilepath(defaultFilename);
       const { found, buffer, size, mime } = fetchLogo(logoPath);
